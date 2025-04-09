feba65666e3b6058effe3c247ee482cbd8661e9e
Max Schipper <max.schipper1@gmx.de>
2025-04-09 14:00:40 +0200

    feat: disable cups printing

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,7 @@
   console.keyMap = "de-latin1-nodeadkeys";
 
   # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # services.printing.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
