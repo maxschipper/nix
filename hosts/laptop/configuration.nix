c823772150ead73079e60227bd27b2bf6280eb56
Max S <max.schipper1@gmx.de>
2025-08-01 17:11:34 +0200

    feat(systemd-boot): make boot faster

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -9,6 +9,9 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.editor = false;
+  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.timeout = 0;
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
