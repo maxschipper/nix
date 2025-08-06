6d094982dd11f9626f1f3efe00dc5efab9b460b3
Max S <max.schipper1@gmx.de>
2025-08-07 00:17:28 +0200

    feat(hypr): add a lot of wayland/hyprland config and hyprland related packages

--- modules/de/hyprland.nix
+++ modules/de/hyprland.nix
@@ -13,14 +13,38 @@
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
   };
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    MOZ_ENABLE_WAYLAND = "1";
+    GDK_BACKEND = "wayland,x11";
+    XDG_SESSION_TYPE = "wayland";
+    QT_QPA_PLATFORM = "wayland";
+  };
 
   # enable the sddm display manager with wayland support
   # services.displayManager.sddm.enable = true;
   # services.displayManager.sddm.wayland.enable = true;
 
   environment.systemPackages = with pkgs; [
+    hyprls
+    hyprpolkitagent
+    hyprland-qt-support
+
+    hyprdim
+    hyprcursor
+    hyprkeys
+    hypridle
+    hyprlock
+    hyprpaper
+    hyprshot
+    hyprsunset
+    hyprsysteminfo
+
+    wl-clipboard
+
     waybar
     wofi
     walker
+    hyprpicker # colorpicker
   ];
 }
