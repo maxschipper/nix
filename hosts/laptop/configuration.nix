c37234beaad8bad56c862a1a3ed9535702cbff5c
Max S <max.schipper1@gmx.de>
2025-08-11 22:16:40 +0200

    feat(sleep): add sleep config disabling suspend

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -16,13 +16,18 @@
 
   services.logind.powerKey = "ignore";
   services.logind.lidSwitch = "hibernate";
-  systemd.sleep.extraConfig = "HibernateDelaySec=1h";
-
+  # systemd.sleep.extraConfig = "HibernateDelaySec=1h";
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=yes
+    AllowHybridSleep=yes
+    AllowSuspendThenHibernate=no
+  '';
   services.auto-cpufreq.enable = true;
   services.auto-cpufreq.settings = {
     battery = {
       governor = "powersave";
-      turbo = "never";
+      turbo = "auto";
     };
     charger = {
       governor = "performance";
@@ -164,16 +169,8 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
