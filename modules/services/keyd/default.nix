2831e6e082ac2f109aedfc83f475e23402c4191f
Max S <150921823+maxschipper@users.noreply.github.com>
2025-08-15 23:33:27 +0200

    feat(keyd): rebind copilot key to right control

--- modules/services/keyd/default.nix
+++ modules/services/keyd/default.nix
@@ -1,8 +1,26 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = [
+    pkgs.keyd
+  ];
+
   services.keyd = {
     enable = true;
+    keyboards.default = {
+      ids = [ "*" ];
+      settings = {
+        main = {
+          "leftshift+leftmeta+f23" = "rightcontrol"; # bind copilot key to right control
+        };
+      };
+    };
   };
+
+  users.groups.keyd.members = [ "max" ];
+  # Override systemd service options
+  systemd.services.keyd.serviceConfig.CapabilityBoundingSet = [
+    "CAP_SETGID"
+  ];
 
   # Optional, but makes sure that when you type the make palm rejection work with keyd
   # https://github.com/rvaiya/keyd/issues/723
