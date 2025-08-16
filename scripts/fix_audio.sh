0f27a84173e28ecb10c2baec382507f7ac422b83
Max S <150921823+maxschipper@users.noreply.github.com>
2025-08-16 16:01:16 +0200

    fix(script): fix audio script with a short wait

--- scripts/fix_audio.sh
+++ scripts/fix_audio.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env sh
 
+# run this after startup or add it to your wm config
+# hyprland.conf: exec-once = .../fix_audio.sh
+
 pactl set-card-profile alsa_card.pci-0000_04_00.6 output:analog-stereo
+sleep 1
 pactl set-card-profile alsa_card.pci-0000_04_00.6 pro-audio
