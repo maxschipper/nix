f475ca15a302b4f16e5794ea56897ca90ac73277
Max S <150921823+maxschipper@users.noreply.github.com>
2025-08-16 15:37:48 +0200

    fix(audio): add audio fix that toggles the audio profile

---
+++ modules/system/audio/fixAudio.nix
@@ -0,0 +1,18 @@
+let
+  vars = ../../vars.nix;
+in
+{
+  systemd.user.services.fixProAudio = {
+    description = "Fix Pro Audio profile for AMD HD Audio";
+    after = [
+      "pipewire.service"
+      "wireplumber.service"
+    ];
+    wantedBy = [ "default.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${vars.nhFlake}/scripts/fix_audio.sh";
+    };
+  };
+}
---
+++ scripts/fix_audio.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env sh
+
+pactl set-card-profile alsa_card.pci-0000_04_00.6 output:analog-stereo
+pactl set-card-profile alsa_card.pci-0000_04_00.6 pro-audio
