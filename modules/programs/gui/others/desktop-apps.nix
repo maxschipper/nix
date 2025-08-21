a5e9a4cbf6b4f831ec62b96c0b2ff7c85161e6a2
Max S <150921823+maxschipper@users.noreply.github.com>
2025-08-21 10:38:28 +0200

    feat(pkg/gui/desktop): add cheese and snapshot as camera apps

--- modules/programs/gui/others/desktop-apps.nix
+++ modules/programs/gui/others/desktop-apps.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    pkgs.snapshot
     pkgs.kdePackages.dolphin
     pkgs.gnome-disk-utility
     pkgs.gnome-calendar
     pkgs.gnome-font-viewer
+    pkgs.cheese
+    pkgs.snapshot
   ];
 }
