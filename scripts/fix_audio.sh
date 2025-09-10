612ca977236628713fcb76b3de9a59aebb948771
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-10 09:54:55 +0200

    fix(audio): update the fix_audio script for new flake lock file
    after new alsa-ucm-conf or pipewire version idk the old fix was no
    longer needed
    
    BUT... now the channels are muted by default in alsamixer

--- scripts/fix_audio.sh
+++ scripts/fix_audio.sh
@@ -3,6 +3,5 @@
 # run this after startup or add it to your wm config
 # hyprland.conf: exec-once = .../fix_audio.sh
 
-pactl set-card-profile alsa_card.pci-0000_04_00.6 output:analog-stereo
-sleep 1
-pactl set-card-profile alsa_card.pci-0000_04_00.6 pro-audio
+amixer -c 1 set Master unmute
+amixer -c 1 set Speaker unmute
