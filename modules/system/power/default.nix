1893c6e57aee836f8ba4607c9b6fa11c7ae5861e
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-14 12:18:59 +0200

    fix(power): fix power button to toggle the screen on and off

--- modules/system/power/default.nix
+++ modules/system/power/default.nix
@@ -27,10 +27,34 @@
     event = "button/power PBTN";
     action = ''
       echo "$(date): Power button pressed to toggle dpms" >> /var/log/powerbutton.log
+
       sig=$(ls -d /run/user/1000/hypr/* | head -n 1 | xargs basename)
+
+      STATE_FILE="/tmp/dpms_state"
+
+      # default state: off
+      if [ ! -f "$STATE_FILE" ]; then
+        echo "off" > "$STATE_FILE"
+      fi
+
+      state=$(cat "$STATE_FILE")
+      echo "$(date): state=$state" >> /var/log/powerbutton.log
+
+
+      if [ "$state" = "on" ]; then
+        newstate="off"
+      else
+        newstate="on"
+      fi
+      echo "$newstate" > "$STATE_FILE"
+      echo "$(date): wrote newstate=$newstate into $STATE_FILE" >> /var/log/powerbutton.log
+
+
+      echo "$(date): DPMS toggling from $state to $newstate" >> /var/log/powerbutton.log
+
       XDG_RUNTIME_DIR="/run/user/1000" \
       HYPRLAND_INSTANCE_SIGNATURE="$sig" \
-      ${pkgs.hyprland}/bin/hyprctl dispatch dpms toggle 2>> /var/log/powerbutton.log
+      ${pkgs.hyprland}/bin/hyprctl dispatch dpms "$newstate" 2>> /var/log/powerbutton.log
     '';
   };
 }
