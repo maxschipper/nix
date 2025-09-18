33dfdbb8d04936c5cc484125bde743eb95832967
Max Schipper <max.schipper1@gmx.de>
2025-09-18 23:37:23 +0200

    remove xserver

--- hosts/imac/devnix/default.nix
+++ hosts/imac/devnix/default.nix
@@ -4,9 +4,7 @@
     ./../common-configuration.nix
     ./../hardware-configuration.nix
     ../../../modules/sessions/wm/cage.nix
-    ../../../modules/sessions/wm/xserver.nix
     ../../../modules/services/tailscale
     ../../../modules/programs/gui/others/moonlight.nix
-    # ../../../modules/programs/gui/browser/chromium.nix
   ];
 }
