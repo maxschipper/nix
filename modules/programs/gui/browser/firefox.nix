22c0e8ba3be888ec6b183e22627e81e016f93fe9
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-18 10:35:09 +0200

    feat(firefox): add pwa support using firefoxpwa

--- modules/programs/gui/browser/firefox.nix
+++ modules/programs/gui/browser/firefox.nix
@@ -1,3 +1,13 @@
+{ pkgs, ... }:
 {
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    nativeMessagingHosts.packages = [
+      pkgs.firefoxpwa
+    ];
+  };
+
+  environment.systemPackages = [
+    pkgs.firefoxpwa
+  ];
 }
