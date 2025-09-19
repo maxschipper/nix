4ea39a838bf01491fcc977c2b68bc1ab9553f73f
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-19 19:04:11 +0200

    fix(cage): add flags to cage to enable tty switching

--- modules/sessions/wm/cage.nix
+++ modules/sessions/wm/cage.nix
@@ -7,5 +7,14 @@
     enable = true;
     user = "max";
     program = "${pkgs.moonlight-qt}/bin/moonlight";
+    extraArguments = [
+      "-s" # Allow VT switching
+      "-d" # Don't draw client side decorations, when possible
+      # "-m extend" # Extend the display across all connected outputs (default)
+      # "-m last" # Use only the last connected output
+    ];
+    environment = {
+      WLR_LIBINPUT_NO_DEVICES = "1"; # to also run without any input devices
+    };
   };
 }
