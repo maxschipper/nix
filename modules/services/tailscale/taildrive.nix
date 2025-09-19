6d5163985cba8c36211974d8c77493932c0ab637
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-19 19:03:45 +0200

    feat(tailscale/taildrive): automount taildrive

--- modules/services/tailscale/taildrive.nix
+++ modules/services/tailscale/taildrive.nix
@@ -13,4 +13,27 @@
       };
     };
   };
+
+  systemd.mounts = [
+    {
+      description = "tailscale taildrive mount";
+      what = "http://100.100.100.100:8080/max.schipper1@icloud.com/devnix/share";
+      where = "/home/max/Documents/imac-share";
+      # where = "/mnt/taildrive-devnix";
+      type = "davfs";
+      options = "rw,uid=1000,gid=100,_netdev";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network-online.target" ];
+      requires = [ "network-online.target" ];
+    }
+  ];
+  environment.etc."davfs2/secrets" = {
+    text = ''
+      http://100.100.100.100:8080/max.schipper1@icloud.com/devnix/share max "" 
+    '';
+    mode = "0600";
+    uid = 0;
+    gid = 0;
+  };
+
 }
