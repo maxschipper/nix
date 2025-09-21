cb4dd357c2c7661131a9ddd03192e8ffea9a3aa9
Max S <150921823+maxschipper@users.noreply.github.com>
2025-09-21 16:22:16 +0200

    feat(niri): add some pkgs

--- modules/sessions/wm/niri.nix
+++ modules/sessions/wm/niri.nix
@@ -9,13 +9,18 @@
   # Required for screen sharing
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gnome ]; # Niri uses the gnome portal
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gnome
+      pkgs.xdg-desktop-portal-gtk
+    ];
   };
 
   environment.systemPackages = [
     pkgs.wl-clipboard
     pkgs.wofi
     pkgs.walker
-    # pkgs.waybar
+    pkgs.xwayland-satellite
+    pkgs.wl-clipboard
+    pkgs.gnome-keyring
   ];
 }
