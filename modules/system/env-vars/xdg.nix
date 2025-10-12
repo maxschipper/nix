3392b552ba13093244defe8d50e4e37d5d79c08f
Max S <150921823+maxschipper@users.noreply.github.com>
2025-10-12 22:26:26 +0200

    feat(env): add env vars for application to use more of the XDG spec

--- modules/system/env-vars/xdg.nix
+++ modules/system/env-vars/xdg.nix
@@ -6,6 +6,15 @@
     # XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
+
+    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    HISTFILE = "$XDG_STATE_HOME/bash/history";
+    GOPATH = "$XDG_DATA_HOME/go";
+    XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java";
+    PARALLEL_HOME = "$XDG_CONFIG_HOME/parallel";
+    PYTHON_HISTORY = "";
+    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
   };
 
 }
