ea258847469ffd67c6dc1485e690e414f1195fe3
Max S <150921823+maxschipper@users.noreply.github.com>
2025-10-25 18:42:47 +0200

    chore(fonts): clean up fonts

--- modules/programs/gui/fonts/default.nix
+++ modules/programs/gui/fonts/default.nix
@@ -1,10 +1,13 @@
 { pkgs, ... }:
 {
   fonts.packages = [
-    pkgs.jetbrains-mono
     pkgs.nerd-fonts.jetbrains-mono
-
     pkgs.noto-fonts
-  ];
+    pkgs.inter
 
+    # pkgs.jetbrains-mono
+    # pkgs.material-design-icons
+    # pkgs.material-icons
+    # pkgs.material-symbols
+  ];
 }
