664b313158a8e2246431a0535d22ca0bb570993d
Max S <150921823+maxschipper@users.noreply.github.com>
2025-10-26 15:06:14 +0100

    fix(caddy/tls): uploaded real cert + fix reading it

--- modules/services/homelab/caddy/tls-cert/trust.nix
+++ modules/services/homelab/caddy/tls-cert/trust.nix
@@ -1,3 +1,7 @@
+# let
+#   vars = import ../../../../vars.nix;
+#   certPath = vars.nhFlake + "/modules/services/homelab/caddy/tls-cert/root.crt";
+# in
 {
-  security.pki.certificateFiles = [ "./root.crt" ];
+  security.pki.certificates = [ (builtins.readFile ./root.crt) ];
 }
