51c835e9a09d6af2db3bd60a663c729cd0a9d57e
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-08 13:14:43 +0100

    refactor(homelab/dns): move coredns to dns module

--- hosts/nuc/default.nix
+++ hosts/nuc/default.nix
@@ -7,7 +7,7 @@
     ../../modules/services/homelab/ttyd/btop.nix
     ../../modules/services/homelab/caddy
     ../../modules/services/homelab/caddy/tls-cert/trust.nix
-    ../../modules/services/homelab/core-dns
+    ../../modules/services/homelab/dns/core-dns.nix
     ../../modules/services/homelab/pdf/stirling-pdf.nix
 
     ../../modules/programs/gui/browser/chromium.nix
