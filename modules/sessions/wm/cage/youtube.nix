e9fb751cf878932541db67853f5beff27acda5fa
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-14 23:12:42 +0100

    feat(cage/youtube): use chromium kiosk flag and fix resolution
    switch to a newer user string to get better resolution
    also no need for a user agent switcher extension anymore

--- modules/sessions/wm/cage/youtube.nix
+++ modules/sessions/wm/cage/youtube.nix
@@ -4,7 +4,7 @@
   services.cage = {
     enable = true;
     user = "max";
-    program = "${pkgs.chromium}/bin/chromium --app='https://youtube.com/tv'";
+    program = "${pkgs.chromium}/bin/chromium --kiosk --enable-extensions --user-agent='Mozilla/5.0 (PS4; Leanback Shell) Gecko/20100101 Firefox/65.0 LeanbackShell/01.00.01.75 Sony PS4/ (PS4, , no, CH)' https://youtube.com/tv";
     # program = "${pkgs.chromium}/bin/chromium";
     extraArguments = [
       "-s" # Allow VT switching
