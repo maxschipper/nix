c278d0e03a1318e6ee4a327c49acb01dc82db0b6
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-17 01:15:33 +0100

    fix(coredns): only bind to tailscale interface so resolved can bind to loopback

--- modules/services/homelab/dns/core-dns.nix
+++ modules/services/homelab/dns/core-dns.nix
@@ -1,13 +1,12 @@
-{ lib, ... }:
 {
-  services.resolved.enable = lib.mkForce false;
-
   services.coredns = {
     enable = true;
     # 100.x.x.x needs to be the tailscale ip so the clients can reach it
     # could add local lan ip above so local devices dont need tailscale but the dns is supplied via tailscale so useless for now, only needed if i add coredns to my routers dns, but dont know if it supports split dns
+    # extraArgs = [ "-dns.port=53" ];
     config = ''
       nuc.lab {
+        bind 100.96.128.41
         hosts {
           100.96.128.41 nuc.lab
           100.96.128.41 paperless.nuc.lab
