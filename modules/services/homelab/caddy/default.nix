d3bcb9397bb26312ad336127bf6a529e24f97382
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-22 00:14:01 +0100

    feat(caddy): refactor config
    move tls internal to a global config at the top
    
    add a redirect for www.nuc.lab and dash.nuc.lab to nuc.lab

--- modules/services/homelab/caddy/default.nix
+++ modules/services/homelab/caddy/default.nix
@@ -1,62 +1,59 @@
 {
   services.caddy = {
     enable = true;
+    logFormat = "level INFO";
+    globalConfig = ''
+      # debug
+      local_certs
+    '';
     extraConfig = ''
+
+      https://fritz.nuc.lab {
+        reverse_proxy 10.0.0.1
+      }
+
       https://nuc.lab {
-        tls internal
         reverse_proxy localhost:5678
+      }
+      https://dash.nuc.lab https://www.nuc.lab {
+        redir https://nuc.lab{uri} permanent
       }
 
       https://gitea.nuc.lab {
-        tls internal
         reverse_proxy localhost:3000
       }
 
       https://paperless.nuc.lab {
-        tls internal
         reverse_proxy localhost:28981
       }
 
       https://btop.nuc.lab {
-        tls internal
         reverse_proxy localhost:7682
       }
 
       https://coredns.nuc.lab {
-        tls internal
         reverse_proxy localhost:8000
       }
 
-      https://fritz.nuc.lab {
-        tls internal
-        reverse_proxy 10.0.0.1
-      }
-
       https://pdf.nuc.lab {
-        tls internal
         reverse_proxy localhost:8592
       }
 
       https://adguard.nuc.lab {
-        tls internal
         reverse_proxy localhost:3001
       }
 
       https://dns.adguard.nuc.lab {
-        tls internal
         reverse_proxy localhost:54
       }
 
       https://todo.nuc.lab {
-        tls internal
         reverse_proxy localhost:3002
       }
 
       https://webdav.nuc.lab {
-        tls internal
         reverse_proxy localhost:2345
       }
-
     '';
   };
 }
