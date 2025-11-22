d517babdf8422c7db2628a07b66ab7d939cef335
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-22 22:47:00 +0100

    feat(monitoring): add nvtop

--- modules/programs/cli/monitoring/default.nix
+++ modules/programs/cli/monitoring/default.nix
@@ -1,13 +1,26 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = [
-    pkgs.amdgpu_top
-    # pkgs.radeontop
-    pkgs.glances
-    pkgs.s0ix-selftest-tool
-    pkgs.linuxKernel.packages.linux_6_17.cpupower
-    pkgs.linuxKernel.packages.linux_6_17.turbostat
-    pkgs.powertop
-    pkgs.bottom
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  environment.systemPackages = lib.mkMerge [
+    [
+      pkgs.glances
+      pkgs.s0ix-selftest-tool
+      pkgs.linuxKernel.packages.linux_6_17.cpupower
+      pkgs.linuxKernel.packages.linux_6_17.turbostat
+      pkgs.powertop
+      pkgs.bottom
+    ]
+    (lib.mkIf (config.networking.hostName == "yoga") [
+      pkgs.amdgpu_top
+      pkgs.radeontop
+      pkgs.nvtopPackages.amd
+    ])
+    (lib.mkIf (config.networking.hostName == "nuc") [
+      pkgs.nvtopPackages.intel
+    ])
   ];
 }
