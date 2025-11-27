f65b1ab17f4823a608b3b9411578e33b536bd21a
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-27 10:47:50 +0100

    feat(immich): init

---
+++ modules/services/homelab/immich/default.nix
@@ -0,0 +1,19 @@
+{
+  services.immich = {
+    enable = true;
+    port = 2283;
+    host = "0.0.0.0";
+    mediaLocation = "/var/lib/immich";
+    # accelerationDevices = [ "/dev/dri/renderD128" ];
+    accelerationDevices = null;
+    machine-learning = {
+      enable = true;
+      environment = {
+        # MACHINE_LEARNING_MODEL_TTL = "600";
+      };
+    };
+    settings = {
+      server.externalDomain = "https://immich.nuc.lab";
+    };
+  };
+}
