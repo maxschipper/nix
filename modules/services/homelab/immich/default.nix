455cdbdce29d6cd3cf9dc6e26e3e9cc04870727f
Max S <150921823+maxschipper@users.noreply.github.com>
2025-11-28 12:56:56 +0100

    feat(immich): add video and render group to immich user
    suggested here: https://wiki.nixos.org/wiki/Immich

--- modules/services/homelab/immich/default.nix
+++ modules/services/homelab/immich/default.nix
@@ -6,14 +6,19 @@
     mediaLocation = "/var/lib/immich";
     # accelerationDevices = [ "/dev/dri/renderD128" ];
     accelerationDevices = null;
-    machine-learning = {
-      enable = true;
-      environment = {
-        # MACHINE_LEARNING_MODEL_TTL = "600";
-      };
-    };
+    # machine-learning = {
+    #   enable = true;
+    #   # environment = {
+    #     # MACHINE_LEARNING_MODEL_TTL = "600";
+    #   # };
+    # };
     settings = {
       server.externalDomain = "https://immich.nuc.lab";
     };
   };
+
+  users.users.immich.extraGroups = [
+    "video"
+    "render"
+  ];
 }
