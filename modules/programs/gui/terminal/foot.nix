33e86a4e0113b9e73faea8a49afd8dcb6cbcf900
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-01 11:26:05 +0100

    feat(foot): change padding

--- modules/programs/gui/terminal/foot.nix
+++ modules/programs/gui/terminal/foot.nix
@@ -5,7 +5,7 @@
     # theme = "catppuccin-macchiato"; # overwrites background color
     settings = {
       main = {
-        pad = "15x15";
+        pad = "10x10";
         font = "JetBrains Mono Nerd Font:size=13";
         dpi-aware = "no";
         bold-text-in-bright = "palette-based";
