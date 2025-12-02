509f0711dc054eceacb1541b27af7be4d059d0a5
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-02 10:47:20 +0100

    feat(chromium): switch to ungoogled chromium

--- modules/programs/gui/browser/chromium.nix
+++ modules/programs/gui/browser/chromium.nix
@@ -1,21 +1,22 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [
-    # pkgs.ungoogled-chromium
-    pkgs.chromium
+    pkgs.ungoogled-chromium
+    # pkgs.chromium
   ];
 
-  # programs.chromium = {
-  #   enable = true;
-  #   extraOpts = {
-  #     "BrowserSignin" = 0;
-  #     "SyncDisabled" = true;
-  #     "PasswordManagerEnabled" = false;
-  #     "SpellcheckEnabled" = false;
-  #   };
-  #   extensions = [
-  #     "ddkjiahejlhfcafbddmgiahcphecmpfh" # ublock origin lite
-  #     "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock
-  #   ];
-  # };
+  programs.chromium = {
+    enable = false;
+    extraOpts = {
+      "BrowserSignin" = 0;
+      "SyncDisabled" = true;
+      "PasswordManagerEnabled" = false;
+      "SpellcheckEnabled" = false;
+    };
+    extensions = [
+      "ocaahdebbfolfmndjeplogmgcagdmblk;https://github.com/NeverDecaf/chromium-web-store/blob/c3c5f43e0b0ad57c4f73c5ae27c43cb69cf777f2/updates.xml" # chrome web store for chromium
+      # "ddkjiahejlhfcafbddmgiahcphecmpfh" # ublock origin lite
+      # "mnjggcdmjocbbbhaepdhchncahnbgone" # sponsorblock
+    ];
+  };
 }
--- scripts/yt-leanback-ui.sh
+++ scripts/yt-leanback-ui.sh
@@ -7,7 +7,7 @@ chromium \
   --disable-infobars \
   --enable-extensions \
   --user-agent="Mozilla/5.0 (Linux; Android 12) Cobalt/22.2.3-gold (PS4)" \
-   https://youtube.com/tv 
+  https://youtube.com/tv 
 
 # other user agents to try:
   # --user-agent="Mozilla/5.0 (PS4; Leanback Shell) Gecko/20100101 Firefox/65.0 LeanbackShell/01.00.01.75 Sony PS4/ (PS4, , no, CH)" \
