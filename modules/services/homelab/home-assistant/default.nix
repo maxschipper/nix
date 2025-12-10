a4e4f29d7bfa52e36b6c3821d0e7e59c230ad7b5
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-10 16:28:23 +0100

    feat(hass): update hass config

--- modules/services/homelab/home-assistant/default.nix
+++ modules/services/homelab/home-assistant/default.nix
@@ -1,6 +1,37 @@
+{ pkgs, ... }:
 {
   services.home-assistant = {
-    enable = false;
+    enable = true;
+    config.http = {
+      server_port = 8123;
+      server_host = "0.0.0.0";
+    };
+    configWritable = true; # but will be overwritten on rebuild
+    # extraArgs = [ "--debug" ];
+    defaultIntegrations = [
+      "application_credentials"
+      "frontend"
+      "hardware"
+      "logger"
+      "network"
+      "system_health"
+      "automation"
+      "person"
+      "scene"
+      "script"
+      "tag"
+      "zone"
+      "counter"
+      "input_boolean"
+      "input_button"
+      "input_datetime"
+      "input_number"
+      "input_select"
+      "input_text"
+      "schedule"
+      "timer"
+      "backup"
+    ];
     extraComponents = [
       "analytics"
       "default_config"
@@ -12,25 +43,27 @@
       # "matter"
       "shelly"
     ];
-    # extraArgs = [ "--debug" ];
-    configWritable = true; # but will be overwritten on rebuild
+    customComponents = with pkgs.home-assistant-custom-components; [
+      # prometheus_sensor
+      # xiaomi_miot
+      smartir
+      samsungtv-smart
+      dwd
+    ];
+
     config = {
-      # homeassistant = {
-      #   name = "Home";
-      #   latitude = "!secret latitude";
-      #   longitude = "!secret longitude";
-      #   elevation = "!secret elevation";
-      #   unit_system = "metric";
-      #   time_zone = "UTC";
-      # };
+      homeassistant = {
+        name = "Nuc";
+        unit_system = "metric";
+        # time_zone = "CET"; # CET for winter and CEST for summer
+        # latitude = "!secret latitude";
+        # longitude = "!secret longitude";
+        # elevation = "!secret elevation";
+      };
       # frontend = {
       #   themes = "!include_dir_merge_named themes";
       # };
-      # feedreader.urls = [ "https://nixos.org/blogs.xml" ];
-      http = {
-        server_port = 8123;
-        server_host = "0.0.0.0";
-      };
+      feedreader.urls = [ "https://nixos.org/blogs.xml" ];
     };
   };
 }
