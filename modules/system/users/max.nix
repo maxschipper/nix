a614b4c6fcba57f99ec0190fd6d523223216a3f6
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-14 00:42:13 +0100

    feat(ssh): add laptop public key

--- modules/system/users/max.nix
+++ modules/system/users/max.nix
@@ -10,5 +10,8 @@
       "input"
       "storage"
     ];
+    openssh.authorizedKeys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUjYI40HHTczZP7f7zS83yQkWtTJsnruKZ7Fny8Mj1/ root@laptop"
+    ];
   };
 }
