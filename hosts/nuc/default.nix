833be5a708f1766a820b2dbec3d33910c93313e5
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-13 23:47:52 +0100

    refactor(homelab/dns)

--- hosts/nuc/default.nix
+++ hosts/nuc/default.nix
@@ -7,8 +7,8 @@
     ../../modules/services/homelab/ttyd/btop.nix
     ../../modules/services/homelab/caddy
     ../../modules/services/homelab/caddy/tls-cert/trust.nix
-    ../../modules/services/homelab/dns/core-dns.nix
-    ../../modules/services/homelab/dns/adguard-home.nix
+    # ../../modules/services/homelab/dns/core-dns
+    ../../modules/services/homelab/dns/adguardhome
     ../../modules/services/homelab/pdf/stirling-pdf.nix
     ../../modules/services/homelab/immich
     ../../modules/services/homelab/home-assistant
--- modules/services/homelab/dns/core-dns/default.nix
+++ modules/services/homelab/dns/core-dns/default.nix
@@ -3,7 +3,7 @@ let
 in
 {
   services.coredns = {
-    enable = false;
+    enable = true;
     # 100.x.x.x needs to be the tailscale ip so the clients can reach it
     # could add local lan ip above so local devices dont need tailscale but the dns is supplied via tailscale so useless for now, only needed if i add coredns to my routers dns, but dont know if it supports split dns
     # extraArgs = [ "-dns.port=53" ];
