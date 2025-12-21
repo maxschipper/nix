4d57cb3d41e400a9e7181e568533347a3e3b43d0
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-21 14:11:05 +0100

    feat(kernelParam): increase gpu gtt table size from 13.5gb to 24gb
    also decreased vram allocation in bios from 4g to 2g

--- modules/system/kernel/params/yoga.nix
+++ modules/system/kernel/params/yoga.nix
@@ -6,6 +6,15 @@
     # "quiet"
     "amd_pstate=active"
     "resume_offset=533760"
+
+    # default 3548010 =~ 13,5gb
+    # 20gb
+    # "ttm.pages_limit=5242880"
+    # "ttm.page_pool_size=5242880"
+    # 24gb
+    "ttm.pages_limit=6291456"
+    "ttm.page_pool_size=6291456"
+
   ];
   boot.resumeDevice = "/dev/mapper/crypted"; # + kenerlParam resume_offset
 }
