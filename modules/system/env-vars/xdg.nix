455c46131383ab54443c1c94ca7df096a0ff0fae
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-25 00:22:25 +0100

    feat(xdg-vars): add some more env vars for programs not using xdg dirs

--- modules/system/env-vars/xdg.nix
+++ modules/system/env-vars/xdg.nix
@@ -1,20 +1,27 @@
 {
   environment.sessionVariables = {
     XDG_CACHE_HOME = "$HOME/.cache";
-    # XDG_CONFIG_DIRS = "/etc/xdg";
     XDG_CONFIG_HOME = "$HOME/.config";
-    # XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
     XDG_DATA_HOME = "$HOME/.local/share";
     XDG_STATE_HOME = "$HOME/.local/state";
 
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    # XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
+    # XDG_CONFIG_DIRS = "/etc/xdg";
+  };
+
+  environment.sessionVariables = {
     HISTFILE = "$XDG_STATE_HOME/bash/history";
-    GOPATH = "$XDG_DATA_HOME/go";
     XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose";
+    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
+    CARGO_HOME = "$XDG_DATA_HOME/cargo";
+    GOPATH = "$XDG_DATA_HOME/go";
     _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java";
     PARALLEL_HOME = "$XDG_CONFIG_HOME/parallel";
     PYTHON_HISTORY = "";
-    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
+    NPM_CONFIG_INIT_MODULE = "$XDG_CONFIG_HOME/npm/config/npm-init.js";
+    NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
+    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
+    DOTNET_CLI_HOME = "$XDG_DATA_HOME/dotnet";
   };
 
 }
