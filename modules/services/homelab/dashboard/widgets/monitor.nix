4452b47219cf62bf30a495ff4d02420d45ca5f0b
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-29 23:28:46 +0100

    feat(dash): reduce timeout to 1s

--- modules/services/homelab/dashboard/widgets/monitor.nix
+++ modules/services/homelab/dashboard/widgets/monitor.nix
@@ -13,28 +13,28 @@ in
       url = "https://gitea.${domain}";
       icon = "sh:gitea";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "Paperless";
       url = "https://paperless.${domain}";
       icon = "sh:paperless-ngx";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "Immich";
       url = "https://immich.${domain}";
       icon = "sh:immich";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "WebDAV";
       url = "https://webdav.${domain}";
       # icon = "sh:file-station";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
       icon = "sh:filesync";
       alt-status-codes = [
         401
@@ -50,35 +50,35 @@ in
       url = "https://adguard.${domain}";
       icon = "sh:adguard-home";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "Home Assistant";
       url = "https://hass.${domain}";
       icon = "sh:home-assistant";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "Navidrome";
       url = "https://navidrome.${domain}";
       icon = "sh:navidrome";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     {
       title = "btop";
       url = "https://btop.${domain}";
       icon = "sh:top";
       same-tab = true;
-      timeout = "5s";
+      timeout = "1s";
     }
     # {
     #   title = "Stirling PDF";
     #   url = "https://pdf.${domain}";
     #   icon = "sh:stirling-pdf";
     #   same-tab = true;
-    #   timeout = "5s";
+    #   timeout = "1s";
     # }
   ];
 }
