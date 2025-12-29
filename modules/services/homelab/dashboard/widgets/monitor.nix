44e358ec5604d07b267c8d37a9d2374a329ed4ca
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-30 00:14:36 +0100

    feat(dash/monitor): init automatic monitor

--- modules/services/homelab/dashboard/widgets/monitor.nix
+++ modules/services/homelab/dashboard/widgets/monitor.nix
@@ -1,5 +1,17 @@
+{ config, lib, ... }:
 let
-  domain = "nuc.lab";
+  cfg = config.homelab;
+
+  enabledServices = lib.filterAttrs (name: svc: svc.enable && svc.monitor.enable) cfg.services;
+
+  monitor = lib.mapAttrsToList (name: svc: {
+    title = name;
+    url = svc.url;
+    icon = svc.monitor.icon;
+    same-tab = true;
+    timeout = "1s";
+  }) enabledServices;
+
 in
 {
   type = "monitor";
@@ -7,78 +19,79 @@ in
   cache = "5s";
   title = "Services";
   # style = "compact";
-  sites = [
-    {
-      title = "Gitea";
-      url = "https://gitea.${domain}";
-      icon = "sh:gitea";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "Paperless";
-      url = "https://paperless.${domain}";
-      icon = "sh:paperless-ngx";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "Immich";
-      url = "https://immich.${domain}";
-      icon = "sh:immich";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "WebDAV";
-      url = "https://webdav.${domain}";
-      # icon = "sh:file-station";
-      same-tab = true;
-      timeout = "1s";
-      icon = "sh:filesync";
-      alt-status-codes = [
-        401
-        207
-      ];
-      # basic-auth = {
-      # username = "super-productivity";
-      # password = "";
-      # };
-    }
-    {
-      title = "Adguard Home";
-      url = "https://adguard.${domain}";
-      icon = "sh:adguard-home";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "Home Assistant";
-      url = "https://hass.${domain}";
-      icon = "sh:home-assistant";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "Navidrome";
-      url = "https://navidrome.${domain}";
-      icon = "sh:navidrome";
-      same-tab = true;
-      timeout = "1s";
-    }
-    {
-      title = "btop";
-      url = "https://btop.${domain}";
-      icon = "sh:top";
-      same-tab = true;
-      timeout = "1s";
-    }
-    # {
-    #   title = "Stirling PDF";
-    #   url = "https://pdf.${domain}";
-    #   icon = "sh:stirling-pdf";
-    #   same-tab = true;
-    #   timeout = "1s";
-    # }
-  ];
+  sites = monitor;
+  # sites = [
+  #   {
+  #     title = "Gitea";
+  #     url = "https://gitea.${domain}";
+  #     icon = "sh:gitea";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "Paperless";
+  #     url = "https://paperless.${domain}";
+  #     icon = "sh:paperless-ngx";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "Immich";
+  #     url = "https://immich.${domain}";
+  #     icon = "sh:immich";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "WebDAV";
+  #     url = "https://webdav.${domain}";
+  #     # icon = "sh:file-station";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #     icon = "sh:filesync";
+  #     alt-status-codes = [
+  #       401
+  #       207
+  #     ];
+  #     # basic-auth = {
+  #     # username = "super-productivity";
+  #     # password = "";
+  #     # };
+  #   }
+  #   {
+  #     title = "Adguard Home";
+  #     url = "https://adguard.${domain}";
+  #     icon = "sh:adguard-home";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "Home Assistant";
+  #     url = "https://hass.${domain}";
+  #     icon = "sh:home-assistant";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "Navidrome";
+  #     url = "https://navidrome.${domain}";
+  #     icon = "sh:navidrome";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   {
+  #     title = "btop";
+  #     url = "https://btop.${domain}";
+  #     icon = "sh:top";
+  #     same-tab = true;
+  #     timeout = "1s";
+  #   }
+  #   # {
+  #   #   title = "Stirling PDF";
+  #   #   url = "https://pdf.${domain}";
+  #   #   icon = "sh:stirling-pdf";
+  #   #   same-tab = true;
+  #   #   timeout = "1s";
+  #   # }
+  # ];
 }
