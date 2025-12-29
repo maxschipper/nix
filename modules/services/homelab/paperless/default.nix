74e93139a623b4223f04626a716e893d37a400d9
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-29 02:25:58 +0100

    refactor: use new options for paperless

--- modules/services/homelab/paperless/default.nix
+++ modules/services/homelab/paperless/default.nix
@@ -1,15 +1,16 @@
+{ config, ... }:
 let
-  domain = "nuc.lab";
+  cfg = config.homelab.services.paperless;
 in
 {
   services.paperless = {
-    enable = true;
-    address = "0.0.0.0";
-    port = 28981; # default
+    enable = cfg.enable;
+    address = cfg.ip;
+    port = cfg.port;
     dataDir = "/var/lib/paperless"; # default
     consumptionDirIsPublic = true;
     settings = {
-      PAPERLESS_URL = "https://paperless.${domain}";
+      PAPERLESS_URL = "https://${cfg.subdomain}.${cfg.basedomain}";
       PAPERLESS_CONSUMER_IGNORE_PATTERN = ".DS_STORE";
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
       # PAPERLESS_OCR_USER_ARGS = "";
