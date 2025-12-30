49bbbe03c913ce9dc1e6eb7e932c991fe26084c4
Max S <150921823+maxschipper@users.noreply.github.com>
2025-12-30 02:08:36 +0100

    feat(power): disable old power button logic

--- modules/system/power/default.nix
+++ modules/system/power/default.nix
@@ -24,38 +24,36 @@
   # services.acpid.powerEventCommands = ''
   #   echo "$(date): Power button pressed" >> /var/log/powerbutton.log
   # '';
-  services.acpid.handlers.powerButton = {
-    event = "button/power PBTN";
-    action = ''
-      echo "$(date): Power button pressed to toggle dpms" >> /var/log/powerbutton.log
+  # services.acpid.handlers.powerButton = {
+  #   event = "button/power PBTN";
+  #   action = ''
+  #     echo "$(date): Power button pressed to toggle dpms" >> /var/log/powerbutton.log
 
-      sig=$(ls -d /run/user/1000/hypr/* | head -n 1 | xargs basename)
+  #     sig=$(ls -d /run/user/1000/hypr/* | head -n 1 | xargs basename)
 
-      STATE_FILE="/tmp/dpms_state"
+  #     STATE_FILE="/tmp/dpms_state"
 
-      # default state: off
-      if [ ! -f "$STATE_FILE" ]; then
-        echo "off" > "$STATE_FILE"
-      fi
+  #     # default state: off
+  #     if [ ! -f "$STATE_FILE" ]; then
+  #       echo "off" > "$STATE_FILE"
+  #     fi
 
-      state=$(cat "$STATE_FILE")
-      echo "$(date): state=$state" >> /var/log/powerbutton.log
+  #     state=$(cat "$STATE_FILE")
+  #     echo "$(date): state=$state" >> /var/log/powerbutton.log
 
+  #     if [ "$state" = "on" ]; then
+  #       newstate="off"
+  #     else
+  #       newstate="on"
+  #     fi
+  #     echo "$newstate" > "$STATE_FILE"
+  #     echo "$(date): wrote newstate=$newstate into $STATE_FILE" >> /var/log/powerbutton.log
 
-      if [ "$state" = "on" ]; then
-        newstate="off"
-      else
-        newstate="on"
-      fi
-      echo "$newstate" > "$STATE_FILE"
-      echo "$(date): wrote newstate=$newstate into $STATE_FILE" >> /var/log/powerbutton.log
+  #     echo "$(date): DPMS toggling from $state to $newstate" >> /var/log/powerbutton.log
 
-
-      echo "$(date): DPMS toggling from $state to $newstate" >> /var/log/powerbutton.log
-
-      XDG_RUNTIME_DIR="/run/user/1000" \
-      HYPRLAND_INSTANCE_SIGNATURE="$sig" \
-      ${pkgs.hyprland}/bin/hyprctl dispatch dpms "$newstate" 2>> /var/log/powerbutton.log
-    '';
-  };
+  #     XDG_RUNTIME_DIR="/run/user/1000" \
+  #     HYPRLAND_INSTANCE_SIGNATURE="$sig" \
+  #     ${pkgs.hyprland}/bin/hyprctl dispatch dpms "$newstate" 2>> /var/log/powerbutton.log
+  #   '';
+  # };
 }
