d5e3dbca9c61995594ba9c86d279691d3999bece
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-01 16:29:54 +0100

    feat(grafana): add prometheus datasource

--- modules/services/homelab/monitoring/grafana/default.nix
+++ modules/services/homelab/monitoring/grafana/default.nix
@@ -1,6 +1,7 @@
 { config, ... }:
 let
   cfg = config.homelab.services.grafana;
+  cfgPrometheus = config.homelab.services.prometheus;
 in
 {
   services.grafana = {
@@ -11,17 +12,35 @@ in
         http_port = cfg.port;
         http_addr = cfg.ip;
         root_url = cfg.url;
-        # enable_gzip = true;
+        enable_gzip = true;
       };
-      # database = {
-      # wal = true;
-      #   type = "sqlite3";
-      #   path = "/var/lib/grafana/grafana.db";
-      # };
+      database = {
+        wal = true;
+        # type = "sqlite3";
+        # path = "/var/lib/grafana/grafana.db";
+      };
       analytics = {
         reporting_enabled = false;
         feedback_links_enabled = false;
       };
+    };
+    provision = {
+      enable = true;
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          access = "proxy";
+          editable = true;
+          url = "http://${cfgPrometheus.ip}:${toString cfgPrometheus.port}";
+        }
+        # {
+        #   name = "Loki";
+        #   type = "loki";
+        #   access = "proxy";
+        #   url = "http://127.0.0.1:3100";
+        # }
+      ];
     };
   };
 }
