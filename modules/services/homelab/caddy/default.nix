cd462636721969f05a6a25cfdb5efc3b13cda40e
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-02 15:51:43 +0100

    feat(caddy): add grace_period as suggested by the caddy nixos module

--- modules/services/homelab/caddy/default.nix
+++ modules/services/homelab/caddy/default.nix
@@ -2,7 +2,7 @@
 let
   domain = config.homelab.domain;
   homePageURL = config.homelab.services.glance.url;
-  gitServerUrl = config.homelab.services.gitea.url;
+  gitServerURL = config.homelab.services.gitea.url;
 in
 {
   imports = [ ./auto.nix ];
@@ -12,21 +12,18 @@ in
   ];
   services.caddy = {
     enable = true;
-    # logFormat = "level INFO";
     logFormat = "level ERROR";
-
-    # TODO: Reload Caddy instead of restarting it when configuration file changes.
-    # If you enable this option, consider setting grace_period to a non-infinite value in services.caddy.globalConfig to prevent Caddy waiting for active connections to finish, which could delay the reload essentially indefinitely.
     globalConfig = ''
       # debug
       # the root.crt is at /var/lib/caddy/.local/share/caddy/pki/authorities/local/root.crt
       local_certs
+      grace_period 5s
     '';
     virtualHosts.${homePageURL}.serverAliases = [
       domain
       "www.${domain}"
     ];
-    virtualHosts.${gitServerUrl}.serverAliases = [ "https://git.${domain}" ];
+    virtualHosts.${gitServerURL}.serverAliases = [ "https://git.${domain}" ];
     virtualHosts."https://fritz.${domain}".extraConfig = "reverse_proxy 10.0.0.1";
   };
 }
