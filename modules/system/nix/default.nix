9a7c95b5dffe9517c374504fd79e55ba76ac8d17
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-03 18:02:07 +0100

    build(nix): add pipe-operators experimental feature

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -1,6 +1,9 @@
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+      "pipe-operators"
+    ];
+  };
 }
