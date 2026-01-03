5c8b93d2c128eae37f560b86f04379214d4822a6
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-03 22:34:12 +0100

    feat(hass): add matter server docker container and the needed firewall ports

--- modules/services/homelab/home-assistant/default.nix
+++ modules/services/homelab/home-assistant/default.nix
@@ -1,11 +1,24 @@
 { config, lib, ... }:
 let
   cfg = config.homelab.services.hass;
+  enableMatter = true;
 in
 {
   config = lib.mkIf cfg.enable {
 
-    networking.firewall.allowedTCPPorts = [ cfg.port ];
+    services.caddy.virtualHosts.${cfg.url}.extraConfig =
+      "reverse_proxy ${cfg.ip}:${toString cfg.port} ";
+
+    networking = {
+      enableIPv6 = enableMatter;
+      firewall = {
+        # Matter Server uses 5580 for the WebSocket API
+        allowedTCPPorts = [ cfg.port ] ++ lib.optionals enableMatter [ 5580 ];
+
+        # mDNS (5353) is also required for discovery
+        allowedUDPPorts = lib.optional enableMatter 5353;
+      };
+    };
 
     virtualisation = {
       docker = {
@@ -15,20 +28,30 @@ in
       oci-containers = {
         backend = "docker";
 
-        containers.homeassistant = lib.mkIf cfg.enable {
-          image = "ghcr.io/home-assistant/home-assistant:stable";
-          extraOptions = [ "--network=host" ]; # Mandatory for device discovery
-          volumes = [
-            "/var/lib/home-assistant:/config"
-            "/etc/localtime:/etc/localtime:ro"
-          ];
-          environment.TZ = "Europe/Berlin";
+        containers = {
+
+          homeassistant = {
+            image = "ghcr.io/home-assistant/home-assistant:stable";
+            extraOptions = [ "--network=host" ]; # Mandatory for device discovery
+            volumes = [
+              "/var/lib/home-assistant:/config"
+              "/etc/localtime:/etc/localtime:ro"
+            ];
+            environment.TZ = "Europe/Berlin";
+          };
+
+          matter-server = lib.mkIf enableMatter {
+            image = "ghcr.io/home-assistant-libs/python-matter-server:stable";
+            # autoStart = true;
+            extraOptions = [ "--network=host" ];
+            volumes = [
+              "/var/lib/matter-server:/data"
+            ];
+          };
+
         };
       };
     };
-
-    services.caddy.virtualHosts.${cfg.url}.extraConfig =
-      "reverse_proxy ${cfg.ip}:${toString cfg.port} ";
 
   };
 }
