0c6fd6ed484240634d7bea9d6e5ed22e547e3f2c
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-03 22:27:07 +0100

    refactor(cli/monitoring): clean up some nix logic for tools based on host

--- modules/programs/cli/monitoring/default.nix
+++ modules/programs/cli/monitoring/default.nix
@@ -5,22 +5,20 @@
   ...
 }:
 {
-  environment.systemPackages = lib.mkMerge [
-    [
-      pkgs.glances
-      pkgs.s0ix-selftest-tool
-      pkgs.linuxKernel.packages.linux_6_18.cpupower
-      pkgs.linuxKernel.packages.linux_6_18.turbostat
-      pkgs.powertop
-      pkgs.bottom
-    ]
-    (lib.mkIf (config.networking.hostName == "yoga") [
-      pkgs.amdgpu_top
-      pkgs.radeontop
-      pkgs.nvtopPackages.amd
-    ])
-    (lib.mkIf (config.networking.hostName == "nuc") [
-      pkgs.nvtopPackages.intel
-    ])
+  environment.systemPackages = [
+    pkgs.glances
+    pkgs.s0ix-selftest-tool
+    pkgs.linuxKernel.packages.linux_6_18.cpupower
+    pkgs.linuxKernel.packages.linux_6_18.turbostat
+    pkgs.powertop
+    pkgs.bottom
+  ]
+  ++ lib.optionals (config.networking.hostName == "yoga") [
+    pkgs.amdgpu_top
+    pkgs.radeontop
+    pkgs.nvtopPackages.amd
+  ]
+  ++ lib.optionals (config.networking.hostName == "nuc") [
+    pkgs.nvtopPackages.intel
   ];
 }
