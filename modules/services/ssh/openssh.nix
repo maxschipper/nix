5602847c5a8503df37c38a7f7df2987718f8061c
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-04 20:54:22 +0100

    refactor(ssh): move authorized keys to openssh module not user

--- modules/services/ssh/openssh.nix
+++ modules/services/ssh/openssh.nix
@@ -1,3 +1,6 @@
 {
   services.openssh.enable = true;
+  users.users.max.openssh.authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUjYI40HHTczZP7f7zS83yQkWtTJsnruKZ7Fny8Mj1/ root@laptop"
+  ];
 }
--- modules/system/users/max.nix
+++ modules/system/users/max.nix
@@ -10,8 +10,5 @@
       "input"
       "storage"
     ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUjYI40HHTczZP7f7zS83yQkWtTJsnruKZ7Fny8Mj1/ root@laptop"
-    ];
   };
 }
