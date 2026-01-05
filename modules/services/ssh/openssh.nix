7ed2d1edbbb794df8fd9a67086a739b5060c2f51
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-05 15:45:53 +0100

    fix(openssh): fix auth keys section

--- modules/services/ssh/openssh.nix
+++ modules/services/ssh/openssh.nix
@@ -1,6 +1,6 @@
 {
   services.openssh.enable = true;
-  users.users.max.openssh.authorizedKeys = [
+  users.users.max.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUjYI40HHTczZP7f7zS83yQkWtTJsnruKZ7Fny8Mj1/ root@laptop"
   ];
 }
