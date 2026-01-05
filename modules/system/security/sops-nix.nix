eba249f28b0b81efaaf3ad19589aa3d622ff1f18
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-05 21:41:29 +0100

    fix(sops): convert paths outside of nix store to strings and only use age key on yoga

--- modules/system/security/sops-nix.nix
+++ modules/system/security/sops-nix.nix
@@ -1,10 +1,17 @@
-{ inputs, ... }:
+{
+  config,
+  inputs,
+  lib,
+  ...
+}:
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   sops.defaultSopsFile = ../../../secrets/secret.yaml;
-  sops.age.keyFile = /home/max/.config/sops/age/key.txt;
-  sops.age.sshKeyPaths = [ /etc/ssh/ssh_host_ed25519_key ];
+  sops.age.keyFile = lib.mkIf (
+    config.networking.hostName == "yoga"
+  ) "/home/max/.config/sops/age/key.txt";
+  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
   sops.age.generateKey = false;
 
   sops.secrets.NETBIRD_API_KEY = {
