49c257c8186b71c005bf1cead7b2ec583ef72d4d
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-08 07:53:22 +0100

    feat(sops): add sops package

--- modules/system/security/sops-nix.nix
+++ modules/system/security/sops-nix.nix
@@ -3,10 +3,12 @@
   flakeRoot,
   inputs,
   lib,
+  pkgs,
   ...
 }:
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
+  environment.systemPackages = with pkgs; [ sops ];
 
   sops.defaultSopsFile = "${flakeRoot}/secrets/secret.yaml";
   sops.age.keyFile = lib.mkIf (
