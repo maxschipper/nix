9c861a12ade7a56b7c95ac691897d6c6764865a3
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-08 07:54:01 +0100

    feat(easyroam): add encrypted p12 secret and use it in easyroam

--- modules/services/vpn/easyroam.nix
+++ modules/services/vpn/easyroam.nix
@@ -1,15 +1,28 @@
-{ flakeRoot, ... }:
 {
-  # NOTE:
-  # https://github.com/einetuer/nix-easyroam#usage
-  # , openssl pkcs12 -in Downloads/yoga.p12 -passin pass: -nokeys | , openssl x509 -noout -subject | sed -e 's/.*=//' -e
-  #  's/\s*//' | wl-copy
-  # identity in nmtui
+  config,
+  flakeRoot,
+  inputs,
+  ...
+}:
+{
   imports = [ inputs.nix-easyroam.nixosModules.nix-easyroam ];
+
+  sops.secrets."easyroam-cert.p12" = {
+    sopsFile = "${flakeRoot}/secrets/easyroam-yoga.p12.enc";
+    format = "binary";
+    restartUnits = [ "easyroam-install.service" ];
+  };
 
   services.easyroam = {
     enable = true;
     networkmanager.enable = true;
-    pkcsFile = "${flakeRoot}/modules/services/vpn/yoga.p12";
+    pkcsFile = config.sops.secrets."easyroam-cert.p12".path;
   };
+
+  # NOTE:
+  # https://github.com/einetuer/nix-easyroam#usage
+  #
+  # , openssl pkcs12 -in Downloads/yoga.p12 -passin pass: -nokeys | , openssl x509 -noout -subject | sed -e 's/.*=//' -e
+  #  's/\s*//' | wl-copy
+  # identity in nmtui
 }
