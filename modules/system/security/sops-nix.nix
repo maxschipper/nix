ae56a175e31245ddffd36e932f6b9cc08a594e48
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-09 19:55:43 +0100

    fix(sops-nix): fix sops-nix failing to decrypt secrets
    sops-nix failed to decrypt secrets because it couldnt find
    sops.age.keyFile because the encrypted partition /home is mounted after
    sops-nix service runs
    
    so the age key is moved to /var/lib/...
    
    and the sops cli env vars are set for the age key and the ssh private
    key

--- modules/system/security/sops-nix.nix
+++ modules/system/security/sops-nix.nix
@@ -1,20 +1,30 @@
 {
-  config,
+  flakeStoreRoot,
   inputs,
-  lib,
   pkgs,
   ...
 }:
+let
+  sopsAgeKeyPath = "/var/lib/sops/age/keys.txt";
+  sshPrivKeyPath = "/etc/ssh/ssh_host_ed25519_key";
+in
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
   environment.systemPackages = with pkgs; [ sops ];
 
-  sops.defaultSopsFile = ../../../secrets/secret.yaml;
-  sops.age.keyFile = lib.mkIf (
-    config.networking.hostName == "yoga"
-  ) "/home/max/.config/sops/age/keys.txt";
-  sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.age.generateKey = false;
+  sops = {
+    defaultSopsFile = flakeStoreRoot + /secrets/secret.yaml;
+    age = {
+      sshKeyPaths = [ sshPrivKeyPath ];
+      keyFile = sopsAgeKeyPath;
+      generateKey = true;
+    };
+  };
 
+  # to let the sops cli know of these keys (can't be done in .sops.yaml)
+  # must run sops with sudo to access these files
+  environment.sessionVariables = {
+    SOPS_AGE_KEY_FILE = sopsAgeKeyPath;
+    SOPS_AGE_SSH_PRIVATE_KEY_FILE = sshPrivKeyPath;
   };
 }
