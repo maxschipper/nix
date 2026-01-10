db69ebd23f0262d67f132e9ab355d3bf326796fd
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-10 14:26:56 +0100

    refactor(caddy/tls): use certificateFiles instead of certificates with readFile

--- modules/services/homelab/caddy/tls-cert/trust.nix
+++ modules/services/homelab/caddy/tls-cert/trust.nix
@@ -1,7 +1,3 @@
-# let
-#   vars = import ../../../../vars.nix;
-#   certPath = vars.nixosConfigPath + "/modules/services/homelab/caddy/tls-cert/root.crt";
-# in
 {
-  security.pki.certificates = [ (builtins.readFile ./root.crt) ];
+  security.pki.certificateFiles = [ ./root.crt ];
 }
