d57118f8fc4ad6676d880b07749748433317e5cf
Max Schipper <max.schipper1@gmx.de>
2026-01-10 18:40:38 +0100

    feat(cli/monitoring): disable most tools

--- modules/programs/cli/monitoring/default.nix
+++ modules/programs/cli/monitoring/default.nix
@@ -6,12 +6,12 @@
 }:
 {
   environment.systemPackages = [
-    pkgs.glances
-    pkgs.s0ix-selftest-tool
-    pkgs.linuxKernel.packages.linux_6_18.cpupower
-    pkgs.linuxKernel.packages.linux_6_18.turbostat
-    pkgs.powertop
-    pkgs.bottom
+    # pkgs.glances
+    # pkgs.s0ix-selftest-tool
+    # pkgs.linuxKernel.packages.linux_6_18.cpupower
+    # pkgs.linuxKernel.packages.linux_6_18.turbostat
+    # pkgs.powertop
+    # pkgs.bottom
   ]
   ++ lib.optionals (config.networking.hostName == "yoga") [
     pkgs.amdgpu_top
