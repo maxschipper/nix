0cf07bc115f1f983a96b3e96242aaf7734ca9b15
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-16 16:52:42 +0100

    feat(orcaslicer): add firewall rules to open ports needed by bambu network plugin
    to use my a1 mini in lan-mode

--- modules/programs/gui/3d/orcaslicer.nix
+++ modules/programs/gui/3d/orcaslicer.nix
@@ -3,4 +3,26 @@
   environment.systemPackages = [
     pkgs.orca-slicer
   ];
+
+  # https://wiki.bambulab.com/en/general/printer-network-ports
+  networking.firewall = {
+    allowedTCPPorts = [
+      8883 # mqtt
+      990 # ftp
+    ];
+    # ftp
+    allowedTCPPortRanges = [
+      {
+        from = 50000;
+        to = 50100;
+      }
+    ];
+
+    # Device discovering SSDP. multicast/broadcast
+    allowedUDPPorts = [
+      # 1990 # docs say this port? but SSDP is 1900
+      1900
+      2021
+    ];
+  };
 }
