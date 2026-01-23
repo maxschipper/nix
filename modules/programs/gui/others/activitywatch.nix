470702c23bba80f75c0dc6426d55ff385fe9f80b
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-23 22:02:08 +0100

    feat(activity-watch): add systemd services instead of launching in niri config

--- modules/programs/gui/others/activitywatch.nix
+++ modules/programs/gui/others/activitywatch.nix
@@ -1,8 +1,34 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [
-    pkgs.activitywatch
-    pkgs.aw-watcher-window-wayland
-    pkgs.awatcher
+  environment.systemPackages = with pkgs; [
+    activitywatch
+    aw-watcher-window-wayland
+    awatcher
   ];
+
+  systemd.user.services.activity-watch = {
+    description = "acitivitywatch aw-server service from nix config";
+
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.activitywatch}/bin/aw-server";
+      Restart = "on-failure";
+    };
+  };
+
+  systemd.user.services.awatcher = {
+    description = "acitivitywatch aw-server service from nix config";
+
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    wantedBy = [ "graphical-session.target" ];
+
+    serviceConfig = {
+      ExecStart = "${pkgs.awatcher}/bin/awatcher";
+      Restart = "on-failure";
+    };
+  };
 }
