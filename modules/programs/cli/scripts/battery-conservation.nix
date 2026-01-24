e20fcc87b67c0e1d8ebad4e2cd0b137a4ef2acf7
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-24 18:56:44 +0100

    feat(battery-toggle): update battery-toggle script

--- modules/programs/cli/scripts/battery-conservation.nix
+++ modules/programs/cli/scripts/battery-conservation.nix
@@ -3,29 +3,37 @@
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "battery-toggle" ''
       # BAT_PATH=$(find /sys/bus/platform/drivers/ideapad_acpi -name "conservation_mode" | head -n 1) # doesnt work??
-      BAT_PATH="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
+      MODE_PATH="/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode"
 
-      if [ -z "$BAT_PATH" ]; then
+      if [ -z "$MODE_PATH" ]; then
         echo "Error: Lenovo conservation mode not found."
         exit 1
       fi
 
-      STATUS=$(cat "$BAT_PATH")
+      MODE_STATUS=$(cat "$MODE_PATH")
 
-      if [ "$1" == "status" ]; then
-        if [ "$STATUS" -eq 1 ]; then
-          echo "Limited (Safe)"
+      if [ "$1" == "-h" ]; then
+        echo "usage: $0 status|toggle|limit|full"
+      elif [ "$1" == "status" ]; then
+        if [ "$MODE_STATUS" -eq 1 ]; then
+          echo "Limited (80%)" # 80% in my case (lenovo yoga 7 2in1 14AKP10)
         else
           echo "Full (100%)"
         fi
-      else
-        if [ "$STATUS" -eq 1 ]; then
-          echo 0 | sudo tee "$BAT_PATH" > /dev/null
+      elif [ "$1" == "toggle" ]; then
+        if [ "$MODE_STATUS" -eq 1 ]; then
+          echo 0 | sudo tee "$MODE_PATH" > /dev/null
           echo "Conservation Mode OFF. Charging to 100%."
         else
-          echo 1 | sudo tee "$BAT_PATH" > /dev/null
+          echo 1 | sudo tee "$MODE_PATH" > /dev/null
           echo "Conservation Mode ON. Battery limited."
         fi
+      elif [ "$1" == "limit" ]; then
+          echo 1 | sudo tee "$MODE_PATH" > /dev/null
+          echo "Conservation Mode ON. Battery limited."
+      elif [ "$1" == "full" ]; then
+          echo 0 | sudo tee "$MODE_PATH" > /dev/null
+          echo "Conservation Mode OFF. Charging to 100%."
       fi
     '')
   ];
