a18e29a08dbb469422451b0db18046dd252290f4
Max Schipper <150921823+maxschipper@users.noreply.github.com>
2026-01-24 20:37:26 +0100

    feat(ssh): add android tablet public key

--- modules/system/users/max/default.nix
+++ modules/system/users/max/default.nix
@@ -13,6 +13,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAU9NWmeaFYU/OhqLLJwKTHxtK91WG3UeebMWVVu+SSK max@yoga"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0CLY5h1CNx2tx4uvfkabFqgAtW31KcZVHQB/6MLDrm max@nuc"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIABviA4sJnOLoK6uqFmmB+5if7TAgFbnawwZmaGx0WMP u0_a235@tablet"
     ];
   };
 }
