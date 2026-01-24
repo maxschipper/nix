fc383029345ae5fd355d3be9e1f91fb7c524f11a
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-24 19:44:46 +0100

    feat(ssh): add max@nuc public ssh key to authorized keys

--- modules/system/users/max/default.nix
+++ modules/system/users/max/default.nix
@@ -12,6 +12,7 @@
     ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAU9NWmeaFYU/OhqLLJwKTHxtK91WG3UeebMWVVu+SSK max@yoga"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0CLY5h1CNx2tx4uvfkabFqgAtW31KcZVHQB/6MLDrm max@nuc"
     ];
   };
 }
