86d05176011b0403813415bd756437949adf084a
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-24 18:38:25 +0100

    feat(user/ssh): add yogas pub ssh key to authorizedKeys for openssh

--- modules/services/ssh/openssh.nix
+++ modules/services/ssh/openssh.nix
@@ -1,6 +1,3 @@
 {
   services.openssh.enable = true;
-  users.users.max.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUjYI40HHTczZP7f7zS83yQkWtTJsnruKZ7Fny8Mj1/ root@laptop"
-  ];
 }
--- modules/system/users/max/default.nix
+++ modules/system/users/max/default.nix
@@ -10,5 +10,8 @@
       "input"
       "storage"
     ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAU9NWmeaFYU/OhqLLJwKTHxtK91WG3UeebMWVVu+SSK max@yoga"
+    ];
   };
 }
