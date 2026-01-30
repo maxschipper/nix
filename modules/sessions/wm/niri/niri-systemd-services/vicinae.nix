2317f7ed7c38139b4167a154f94d35a821f3e819
Max S <150921823+maxschipper@users.noreply.github.com>
2026-01-30 14:26:23 +0100

    fix(vicinae): add nixpkgs overlay to fix vicinae service
    see pr #485323 (https://github.com/NixOS/nixpkgs/pull/485323)

--- modules/sessions/wm/niri/niri-systemd-services/vicinae.nix
+++ modules/sessions/wm/niri/niri-systemd-services/vicinae.nix
@@ -3,4 +3,16 @@
   environment.systemPackages = with pkgs; [ vicinae ];
 
   systemd.user.targets.graphical-session.wants = [ "vicinae.service" ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      vicinae = prev.vicinae.overrideAttrs (old: {
+        postFixup = ''
+          substituteInPlace $out/share/systemd/user/vicinae.service \
+            --replace-fail "/bin/kill" "${prev.coreutils}/bin/kill" \
+            --replace-fail "ExecStart=vicinae" "ExecStart=$out/bin/vicinae"
+        '';
+      });
+    })
+  ];
 }
