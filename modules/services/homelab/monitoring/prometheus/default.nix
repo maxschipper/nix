0a70d54d564b7d340e052405180a7c0609ebe7fb
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-06 09:41:42 +0100

    refactor(prometheus): move scrape configs to own file

--- modules/services/homelab/monitoring/prometheus/default.nix
+++ modules/services/homelab/monitoring/prometheus/default.nix
@@ -1,21 +1,10 @@
-{ config, lib, ... }:
+{ config, ... }:
 let
   cfg = config.homelab.services.prometheus;
-  cfgNode = config.homelab.services.node-exporter;
-  cfgSmart = config.homelab.services.smart-exporter;
-  cfgNavidrome = config.homelab.services.navidrome;
-  cfgNodeLaptop = {
-    enable = true;
-    ip = "100.64.0.161";
-    port = 9100;
-  };
-  cfgCaddy = {
-    enable = true;
-    port = 2019;
-  };
-  cfgAdguard = config.homelab.services.adguard-exporter;
 in
 {
+  imports = [ ./scrapeConfigs.nix ];
+
   services.prometheus = {
     enable = cfg.enable;
     port = cfg.port;
@@ -23,36 +12,6 @@ in
     stateDir = "prometheus2"; # will be under /var/lib/
     rules = [ ];
     globalConfig.scrape_interval = "15s";
-    scrapeConfigs =
-      lib.optional (cfgNode.enable || cfgNodeLaptop.enable) {
-        job_name = "node";
-        static_configs =
-          (lib.optional cfgNode.enable {
-            targets = [ "${cfgNode.ip}:${toString cfgNode.port}" ];
-          })
-          ++ (lib.optional cfgNodeLaptop.enable {
-            targets = [ "${cfgNodeLaptop.ip}:${toString cfgNodeLaptop.port}" ];
-          });
-      }
-      # ++ lib.optional cfgNodeLaptop.enable {
-      #   job_name = "nodeLaptop";
-      #   static_configs = [ { targets = [ "${cfgNodeLaptop.ip}:${toString cfgNodeLaptop.port}" ]; } ];
-      # }
-      ++ lib.optional cfgSmart.enable {
-        job_name = "smart";
-        static_configs = [ { targets = [ "${cfgSmart.ip}:${toString cfgSmart.port}" ]; } ];
-      }
-      ++ lib.optional cfgNavidrome.enable {
-        job_name = "navidrome";
-        static_configs = [ { targets = [ "${cfgNavidrome.ip}:${toString cfgNavidrome.port}" ]; } ];
-      }
-      ++ lib.optional cfgCaddy.enable {
-        job_name = "caddy";
-        static_configs = [ { targets = [ "localhost:${toString cfgCaddy.port}" ]; } ];
-      }
-      ++ lib.optional cfgAdguard.enable {
-        job_name = "adguard";
-        static_configs = [ { targets = [ "localhost:${toString cfgAdguard.port}" ]; } ];
-      };
+    scrapeConfigs = [ ]; # defined in ./scrapeConfigs.nix
   };
 }
---
+++ modules/services/homelab/monitoring/prometheus/scrapeConfigs.nix
@@ -0,0 +1,51 @@
+{ config, lib, ... }:
+let
+  cfgNode = config.homelab.services.node-exporter;
+  cfgSmart = config.homelab.services.smart-exporter;
+  cfgNavidrome = config.homelab.services.navidrome;
+  cfgNodeLaptop = {
+    enable = true;
+    ip = "100.64.0.161";
+    port = 9100;
+  };
+  cfgCaddy = {
+    enable = true;
+    port = 2019;
+  };
+  cfgAdguard = config.homelab.services.adguard-exporter;
+in
+{
+  services.prometheus = {
+    scrapeConfigs =
+      lib.optional (cfgNode.enable || cfgNodeLaptop.enable) {
+        job_name = "node";
+        static_configs =
+          (lib.optional cfgNode.enable {
+            targets = [ "${cfgNode.ip}:${toString cfgNode.port}" ];
+          })
+          ++ (lib.optional cfgNodeLaptop.enable {
+            targets = [ "${cfgNodeLaptop.ip}:${toString cfgNodeLaptop.port}" ];
+          });
+      }
+      # ++ lib.optional cfgNodeLaptop.enable {
+      #   job_name = "nodeLaptop";
+      #   static_configs = [ { targets = [ "${cfgNodeLaptop.ip}:${toString cfgNodeLaptop.port}" ]; } ];
+      # }
+      ++ lib.optional cfgSmart.enable {
+        job_name = "smart";
+        static_configs = [ { targets = [ "${cfgSmart.ip}:${toString cfgSmart.port}" ]; } ];
+      }
+      ++ lib.optional cfgNavidrome.enable {
+        job_name = "navidrome";
+        static_configs = [ { targets = [ "${cfgNavidrome.ip}:${toString cfgNavidrome.port}" ]; } ];
+      }
+      ++ lib.optional cfgCaddy.enable {
+        job_name = "caddy";
+        static_configs = [ { targets = [ "localhost:${toString cfgCaddy.port}" ]; } ];
+      }
+      ++ lib.optional cfgAdguard.enable {
+        job_name = "adguard";
+        static_configs = [ { targets = [ "localhost:${toString cfgAdguard.port}" ]; } ];
+      };
+  };
+}
