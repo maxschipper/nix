89ebbf768b2dd98c04e448d19f240fb3992f4ce2
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-08 16:07:49 +0100

    refactor(hosts): use flakeStoreRoot instead of relative imports

--- hosts/nuc/default.nix
+++ hosts/nuc/default.nix
@@ -1,3 +1,4 @@
+{ flakeStoreRoot, ... }:
 {
   networking.hostName = "nuc";
   imports = [
@@ -7,92 +8,92 @@
 
     # modules/SYSTEM
     ################
-    ../../modules/system/kernel/latest.nix
-    # ../../modules/system/kernel/zen.nix
-    ../../modules/system/kernel/params/nuc.nix
+    (flakeStoreRoot + /modules/system/kernel/latest.nix)
+    # (flakeStoreRoot + /modules/system/kernel/zen.nix)
+    (flakeStoreRoot + /modules/system/kernel/params/nuc.nix)
 
-    ../../modules/system/bootloader/systemd.nix
-    ../../modules/system/initrd/systemd.nix
-    # ../../modules/system/initrd/luks-fido2.nix
+    (flakeStoreRoot + /modules/system/bootloader/systemd.nix)
+    (flakeStoreRoot + /modules/system/initrd/systemd.nix)
+    # (flakeStoreRoot + /modules/system/initrd/luks-fido2.nix)
 
-    ../../modules/system/networking/networkmanager.nix
-    ../../modules/system/networking/resolved.nix
-    ../../modules/system/networking/bluetooth.nix
-    ../../modules/system/nix
-    ../../modules/system/nix/comma.nix
-    ../../modules/system/nix/nh.nix
-    ../../modules/system/nix/nixos-cli.nix
-    ../../modules/system/nix/nix-lsps.nix
-    # ../../modules/system/nix/nix-ld.nix
-    ../../modules/system/audio
-    ../../modules/system/localization
-    ../../modules/system/users/max
-    # ../../modules/system/security/nitrokey.nix
-    # ../../modules/system/security/tpm.nix
-    ../../modules/system/security/sops-nix.nix
-    # ../../modules/system/power # (laptop specific)
-    ../../modules/system/xdg/baseDirSpec
+    (flakeStoreRoot + /modules/system/networking/networkmanager.nix)
+    (flakeStoreRoot + /modules/system/networking/resolved.nix)
+    (flakeStoreRoot + /modules/system/networking/bluetooth.nix)
+    (flakeStoreRoot + /modules/system/nix)
+    (flakeStoreRoot + /modules/system/nix/comma.nix)
+    (flakeStoreRoot + /modules/system/nix/nh.nix)
+    (flakeStoreRoot + /modules/system/nix/nixos-cli.nix)
+    (flakeStoreRoot + /modules/system/nix/nix-lsps.nix)
+    # (flakeStoreRoot + /modules/system/nix/nix-ld.nix)
+    (flakeStoreRoot + /modules/system/audio)
+    (flakeStoreRoot + /modules/system/localization)
+    (flakeStoreRoot + /modules/system/users/max)
+    # (flakeStoreRoot + /modules/system/security/nitrokey.nix)
+    # (flakeStoreRoot + /modules/system/security/tpm.nix)
+    (flakeStoreRoot + /modules/system/security/sops-nix.nix)
+    # (flakeStoreRoot + /modules/system/power # (laptop specific))
+    (flakeStoreRoot + /modules/system/xdg/baseDirSpec)
 
     # modules/SESSIONS
     ##################
-    # ../../modules/sessions/dm/ly.nix
-    # ../../modules/sessions/dm/greetd/tuigreet.nix
-    # ../../modules/sessions/wm/hyprland.nix
-    # ../../modules/sessions/wm/niri.nix
+    # (flakeStoreRoot + /modules/sessions/dm/ly.nix)
+    # (flakeStoreRoot + /modules/sessions/dm/greetd/tuigreet.nix)
+    # (flakeStoreRoot + /modules/sessions/wm/hyprland.nix)
+    # (flakeStoreRoot + /modules/sessions/wm/niri.nix)
 
     # modules/SERVICES
     ##################
-    ../../modules/services/monitoring/glances
-    # ../../modules/services/printing
-    ../../modules/services/remap/keyd.nix
-    ../../modules/services/remap/makima.nix
-    ../../modules/services/ssh/keyAuth.nix
-    ../../modules/services/ssh/openssh.nix
-    # ../../modules/services/streaming/airplay.nix
-    # ../../modules/services/streaming/sunshine.nix
-    # ../../modules/services/virt/distrobox.nix
-    # ../../modules/services/virt/podman.nix
-    # ../../modules/services/vpn/easyroam.nix
-    # ../../modules/services/vpn/eduvpn.nix
-    ../../modules/services/vpn/netbird/server.nix
-    # ../../modules/services/vpn/tailscale
-    # ../../modules/services/vpn/tailscale/exit-node-server.nix # conflicts with netbird/server.nix
-    ../../modules/services/vpn/tailscale/subnet-router.nix
-    # ../../modules/services/vpn/tailscale/taildrive.nix
+    (flakeStoreRoot + /modules/services/monitoring/glances)
+    # (flakeStoreRoot + /modules/services/printing)
+    (flakeStoreRoot + /modules/services/remap/keyd.nix)
+    (flakeStoreRoot + /modules/services/remap/makima.nix)
+    (flakeStoreRoot + /modules/services/ssh/keyAuth.nix)
+    (flakeStoreRoot + /modules/services/ssh/openssh.nix)
+    # (flakeStoreRoot + /modules/services/streaming/airplay.nix)
+    # (flakeStoreRoot + /modules/services/streaming/sunshine.nix)
+    # (flakeStoreRoot + /modules/services/virt/distrobox.nix)
+    # (flakeStoreRoot + /modules/services/virt/podman.nix)
+    # (flakeStoreRoot + /modules/services/vpn/easyroam.nix)
+    # (flakeStoreRoot + /modules/services/vpn/eduvpn.nix)
+    (flakeStoreRoot + /modules/services/vpn/netbird/server.nix)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale/exit-node-server.nix # conflicts with netbird/server.nix)
+    (flakeStoreRoot + /modules/services/vpn/tailscale/subnet-router.nix)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale/taildrive.nix)
 
     # modules/PROGRAMS
     ##################
-    ../../modules/programs/cli
-    ../../modules/programs/cli/shell/fish.nix
-    ../../modules/programs/cli/shell/starship.nix
+    (flakeStoreRoot + /modules/programs/cli)
+    (flakeStoreRoot + /modules/programs/cli/shell/fish.nix)
+    (flakeStoreRoot + /modules/programs/cli/shell/starship.nix)
 
-    # ../../modules/programs/gui/others
-    # ../../modules/programs/gui/others/activitywatch.nix
-    # ../../modules/programs/gui/terminal/foot.nix
-    # ../../modules/programs/gui/browser/firefox.nix
-    # ../../modules/programs/gui/mail/thunderbird.nix
-    # ../../modules/programs/gui/passwordmanager/bitwarden.nix
-    # ../../modules/programs/gui/passwordmanager/ente-auth.nix
-    # ../../modules/programs/gui/virtualization/virt-manager.nix
-    # ../../modules/programs/gui/virtualization/waydroid.nix
-    # ../../modules/programs/gui/office/libreoffice.nix
-    # ../../modules/programs/gui/fonts
-    # ../../modules/programs/gui/cursors
-    # ../../modules/programs/gui/desktop-shell/quickshell.nix
-    # ../../modules/programs/gui/desktop-shell/ignis.nix
+    # (flakeStoreRoot + /modules/programs/gui/others)
+    # (flakeStoreRoot + /modules/programs/gui/others/activitywatch.nix)
+    # (flakeStoreRoot + /modules/programs/gui/terminal/foot.nix)
+    # (flakeStoreRoot + /modules/programs/gui/browser/firefox.nix)
+    # (flakeStoreRoot + /modules/programs/gui/mail/thunderbird.nix)
+    # (flakeStoreRoot + /modules/programs/gui/passwordmanager/bitwarden.nix)
+    # (flakeStoreRoot + /modules/programs/gui/passwordmanager/ente-auth.nix)
+    # (flakeStoreRoot + /modules/programs/gui/virtualization/virt-manager.nix)
+    # (flakeStoreRoot + /modules/programs/gui/virtualization/waydroid.nix)
+    # (flakeStoreRoot + /modules/programs/gui/office/libreoffice.nix)
+    # (flakeStoreRoot + /modules/programs/gui/fonts)
+    # (flakeStoreRoot + /modules/programs/gui/cursors)
+    # (flakeStoreRoot + /modules/programs/gui/desktop-shell/quickshell.nix)
+    # (flakeStoreRoot + /modules/programs/gui/desktop-shell/ignis.nix)
 
     # modules/DEVELOPMENT
     #####################
-    # ../../modules/development/langs/default-lsps.nix
-    # ../../modules/development/typst.nix
+    # (flakeStoreRoot + /modules/development/langs/default-lsps.nix)
+    # (flakeStoreRoot + )
 
     # devtools
     #####################
-    ../../modules/development/tools/jj.nix
-    ../../modules/development/tools/direnv.nix
+    (flakeStoreRoot + /modules/development/tools/jj.nix)
+    (flakeStoreRoot + /modules/development/tools/direnv.nix)
 
     # games
     #####################
-    # ../../modules/programs/gui/games/minecraft.nix
+    # (flakeStoreRoot + /modules/programs/gui/games/minecraft.nix)
   ];
 }
--- hosts/yoga/default.nix
+++ hosts/yoga/default.nix
@@ -1,3 +1,4 @@
+{ flakeStoreRoot, ... }:
 {
   networking.hostName = "yoga";
   imports = [
@@ -7,124 +8,124 @@
 
     # modules/SYSTEM
     ################
-    ../../modules/system/kernel/latest.nix
-    # ../../modules/system/kernel/zen.nix
-    ../../modules/system/kernel/params/yoga.nix
+    (flakeStoreRoot + /modules/system/kernel/latest.nix)
+    #(flakeStoreRoot + /modules/system/kernel/zen.nix)
+    (flakeStoreRoot + /modules/system/kernel/params/yoga.nix)
 
-    ../../modules/system/bootloader/systemd.nix
-    ../../modules/system/initrd/systemd.nix
-    # ../../modules/system/initrd/luks-fido2.nix
+    (flakeStoreRoot + /modules/system/bootloader/systemd.nix)
+    (flakeStoreRoot + /modules/system/initrd/systemd.nix)
+    # (flakeStoreRoot + /modules/system/initrd/luks-fido2.nix)
 
-    ../../modules/system/networking/networkmanager.nix
-    ../../modules/system/networking/resolved.nix
-    ../../modules/system/networking/bluetooth.nix
-    ../../modules/system/nix
-    ../../modules/system/nix/comma.nix
-    ../../modules/system/nix/nh.nix
-    ../../modules/system/nix/nixos-cli.nix
-    ../../modules/system/nix/nix-lsps.nix
-    ../../modules/system/nix/nix-ld.nix
-    ../../modules/system/nix/rippkgs.nix
-    ../../modules/system/audio
-    ../../modules/system/localization
-    ../../modules/system/users/max
-    ../../modules/system/security/sops-nix.nix
-    # ../../modules/system/security/nitrokey.nix
-    ../../modules/system/security/tpm.nix
-    ../../modules/system/power
-    ../../modules/system/tty/tty.nix
-    ../../modules/system/xdg/baseDirSpec
-    ../../modules/system/xdg/defaultApps.nix
+    (flakeStoreRoot + /modules/system/networking/networkmanager.nix)
+    (flakeStoreRoot + /modules/system/networking/resolved.nix)
+    (flakeStoreRoot + /modules/system/networking/bluetooth.nix)
+    (flakeStoreRoot + /modules/system/nix)
+    (flakeStoreRoot + /modules/system/nix/comma.nix)
+    (flakeStoreRoot + /modules/system/nix/nh.nix)
+    (flakeStoreRoot + /modules/system/nix/nixos-cli.nix)
+    (flakeStoreRoot + /modules/system/nix/nix-lsps.nix)
+    (flakeStoreRoot + /modules/system/nix/nix-ld.nix)
+    (flakeStoreRoot + /modules/system/nix/rippkgs.nix)
+    (flakeStoreRoot + /modules/system/audio)
+    (flakeStoreRoot + /modules/system/localization)
+    (flakeStoreRoot + /modules/system/users/max)
+    (flakeStoreRoot + /modules/system/security/sops-nix.nix)
+    # (flakeStoreRoot + /modules/system/security/nitrokey.nix)
+    (flakeStoreRoot + /modules/system/security/tpm.nix)
+    (flakeStoreRoot + /modules/system/power)
+    (flakeStoreRoot + /modules/system/tty/tty.nix)
+    (flakeStoreRoot + /modules/system/xdg/baseDirSpec)
+    (flakeStoreRoot + /modules/system/xdg/defaultApps.nix)
 
     # modules/SESSIONS
     ##################
-    ../../modules/sessions/dm/ly.nix
-    # ../../modules/sessions/dm/greetd/tuigreet.nix
-    # ../../modules/sessions/wm/hyprland
-    ../../modules/sessions/wm/niri
+    (flakeStoreRoot + /modules/sessions/dm/ly.nix)
+    # (flakeStoreRoot + /modules/sessions/dm/greetd/tuigreet.nix)
+    # (flakeStoreRoot + /modules/sessions/wm/hyprland)
+    (flakeStoreRoot + /modules/sessions/wm/niri)
 
     # modules/SERVICES
     ##################
 
-    ../../modules/services/homelab/monitoring/prometheus/exporters/node/external.nix
-    ../../modules/services/printing
-    ../../modules/services/remap/keyd.nix
-    ../../modules/services/remap/makima.nix
-    ../../modules/services/ssh/keyAuth.nix
-    ../../modules/services/ssh/openssh.nix
-    ../../modules/services/streaming/airplay.nix
-    ../../modules/services/streaming/sunshine.nix
-    ../../modules/services/virt/distrobox.nix
-    ../../modules/services/virt/podman.nix
-    ../../modules/services/vpn/easyroam.nix
-    # ../../modules/services/vpn/eduvpn.nix
-    ../../modules/services/vpn/netbird/client.nix
-    # ../../modules/services/vpn/tailscale
-    # ../../modules/services/vpn/tailscale/exit-node-client.nix
-    # ../../modules/services/vpn/tailscale/taildrive.nix
-    ../../modules/system/sensors/iio.nix
+    (flakeStoreRoot + /modules/services/homelab/monitoring/prometheus/exporters/node/external.nix)
+    (flakeStoreRoot + /modules/services/printing)
+    (flakeStoreRoot + /modules/services/remap/keyd.nix)
+    (flakeStoreRoot + /modules/services/remap/makima.nix)
+    (flakeStoreRoot + /modules/services/ssh/keyAuth.nix)
+    (flakeStoreRoot + /modules/services/ssh/openssh.nix)
+    (flakeStoreRoot + /modules/services/streaming/airplay.nix)
+    (flakeStoreRoot + /modules/services/streaming/sunshine.nix)
+    (flakeStoreRoot + /modules/services/virt/distrobox.nix)
+    (flakeStoreRoot + /modules/services/virt/podman.nix)
+    (flakeStoreRoot + /modules/services/vpn/easyroam.nix)
+    # (flakeStoreRoot + /modules/services/vpn/eduvpn.nix)
+    (flakeStoreRoot + /modules/services/vpn/netbird/client.nix)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale/exit-node-client.nix)
+    # (flakeStoreRoot + /modules/services/vpn/tailscale/taildrive.nix)
+    (flakeStoreRoot + /modules/system/sensors/iio.nix)
 
     # modules/PROGRAMS
     ##################
-    ../../modules/programs/laptop
-    # ../../modules/programs/flatpak
+    (flakeStoreRoot + /modules/programs/laptop)
+    # (flakeStoreRoot + /modules/programs/flatpak)
 
-    ../../modules/programs/cli
-    ../../modules/programs/cli/others/gemini.nix
-    ../../modules/programs/cli/shell/fish.nix
-    ../../modules/programs/cli/shell/starship.nix
-    ../../modules/programs/cli/llm/lmstudio
-    # ../../modules/programs/cli/llm/opencode
+    (flakeStoreRoot + /modules/programs/cli)
+    (flakeStoreRoot + /modules/programs/cli/others/gemini.nix)
+    (flakeStoreRoot + /modules/programs/cli/shell/fish.nix)
+    (flakeStoreRoot + /modules/programs/cli/shell/starship.nix)
+    (flakeStoreRoot + /modules/programs/cli/llm/lmstudio)
+    # (flakeStoreRoot + /modules/programs/cli/llm/opencode)
 
-    ../../modules/programs/cli/scripts/battery-conservation.nix
+    (flakeStoreRoot + /modules/programs/cli/scripts/battery-conservation.nix)
 
-    ../../modules/programs/gui/3d/freecad.nix
-    ../../modules/programs/gui/3d/openscad.nix
-    ../../modules/programs/gui/3d/orcaslicer.nix
-    ../../modules/programs/gui/audio
-    ../../modules/programs/gui/browser/chromium.nix
-    ../../modules/programs/gui/browser/firefox.nix
-    ../../modules/programs/gui/browser/qute.nix
-    ../../modules/programs/gui/cursors
-    ../../modules/programs/gui/editor/zed.nix
-    ../../modules/programs/gui/fonts
-    ../../modules/programs/gui/fonts/ibm-plex-mono.nix
-    ../../modules/programs/gui/fonts/iosevka.nix
-    ../../modules/programs/gui/mail/thunderbird.nix
-    # ../../modules/programs/gui/office/libreoffice.nix
-    ../../modules/programs/gui/others
-    ../../modules/programs/gui/others/activitywatch.nix
-    ../../modules/programs/gui/others/desktop-apps.nix
-    # ../../modules/programs/gui/others/livebook.nix
-    ../../modules/programs/gui/others/moonlight.nix
-    ../../modules/programs/gui/passwordmanager/bitwarden.nix
-    ../../modules/programs/gui/passwordmanager/ente-auth.nix
-    ../../modules/programs/gui/terminal/foot.nix
-    ../../modules/programs/gui/virtualization/virt-manager.nix
-    # ../../modules/programs/gui/virtualization/waydroid.nix
+    (flakeStoreRoot + /modules/programs/gui/3d/freecad.nix)
+    (flakeStoreRoot + /modules/programs/gui/3d/openscad.nix)
+    (flakeStoreRoot + /modules/programs/gui/3d/orcaslicer.nix)
+    (flakeStoreRoot + /modules/programs/gui/audio)
+    (flakeStoreRoot + /modules/programs/gui/browser/chromium.nix)
+    (flakeStoreRoot + /modules/programs/gui/browser/firefox.nix)
+    (flakeStoreRoot + /modules/programs/gui/browser/qute.nix)
+    (flakeStoreRoot + /modules/programs/gui/cursors)
+    (flakeStoreRoot + /modules/programs/gui/editor/zed.nix)
+    (flakeStoreRoot + /modules/programs/gui/fonts)
+    (flakeStoreRoot + /modules/programs/gui/fonts/ibm-plex-mono.nix)
+    (flakeStoreRoot + /modules/programs/gui/fonts/iosevka.nix)
+    (flakeStoreRoot + /modules/programs/gui/mail/thunderbird.nix)
+    # (flakeStoreRoot + /modules/programs/gui/office/libreoffice.nix)
+    (flakeStoreRoot + /modules/programs/gui/others)
+    (flakeStoreRoot + /modules/programs/gui/others/activitywatch.nix)
+    (flakeStoreRoot + /modules/programs/gui/others/desktop-apps.nix)
+    # (flakeStoreRoot + /modules/programs/gui/others/livebook.nix)
+    (flakeStoreRoot + /modules/programs/gui/others/moonlight.nix)
+    (flakeStoreRoot + /modules/programs/gui/passwordmanager/bitwarden.nix)
+    (flakeStoreRoot + /modules/programs/gui/passwordmanager/ente-auth.nix)
+    (flakeStoreRoot + /modules/programs/gui/terminal/foot.nix)
+    (flakeStoreRoot + /modules/programs/gui/virtualization/virt-manager.nix)
+    # (flakeStoreRoot + /modules/programs/gui/virtualization/waydroid.nix)
 
-    # ../../modules/programs/gui/desktop-shell/quickshell.nix
-    # ../../modules/programs/gui/desktop-shell/ignis.nix
+    # (flakeStoreRoot + /modules/programs/gui/desktop-shell/quickshell.nix)
+    # (flakeStoreRoot + /modules/programs/gui/desktop-shell/ignis.nix)
 
     # development/langs
     #####################
-    ../../modules/development/port5432.nix
-    ../../modules/development/langs/default-lsps.nix
-    ../../modules/development/langs/typst
-    ../../modules/development/langs/fish
-    ../../modules/development/langs/python
-    ../../modules/development/langs/typescript/bun.nix
+    (flakeStoreRoot + /modules/development/port5432.nix)
+    (flakeStoreRoot + /modules/development/langs/default-lsps.nix)
+    (flakeStoreRoot + /modules/development/langs/typst)
+    (flakeStoreRoot + /modules/development/langs/fish)
+    (flakeStoreRoot + /modules/development/langs/python)
+    (flakeStoreRoot + /modules/development/langs/typescript/bun.nix)
 
     # development/tools
     #####################
-    ../../modules/development/tools/jj.nix
-    ../../modules/development/tools/direnv.nix
-    ../../modules/development/tools/jq.nix
+    (flakeStoreRoot + /modules/development/tools/jj.nix)
+    (flakeStoreRoot + /modules/development/tools/direnv.nix)
+    (flakeStoreRoot + /modules/development/tools/jq.nix)
 
     # games
     #####################
-    ../../modules/programs/gui/games/minecraft.nix
+    (flakeStoreRoot + /modules/programs/gui/games/minecraft.nix)
 
-    ../../modules/services/homelab/caddy/tls-cert/trust.nix
+    (flakeStoreRoot + /modules/services/homelab/caddy/tls-cert/trust.nix)
   ];
 }
