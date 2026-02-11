e4fe58dc2ef2146ac82c1efaca1e5b7df6dbf974
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-11 22:34:31 +0100

    feat(kiosk): switch to wayland (cage) for youtube kiosk

--- hosts/nuc/homelab.nix
+++ hosts/nuc/homelab.nix
@@ -6,8 +6,13 @@
     (flakeStoreRoot + /nixos/services/homelab/caddy/tls-cert/trust.nix)
 
     # (flakeStoreRoot + /nixos/sessions/wm/cage/youtube.nix)
-    (flakeStoreRoot + /nixos/programs/gui/browser/chromium.nix)
+
+    # (flakeStoreRoot + /nixos/programs/gui/browser/chromium.nix)
+
     # (flakeStoreRoot + /nixos/sessions/wm/xserver/matchbox.nix)
+
+    (flakeStoreRoot + /nixos/sessions/wm/wayland/cage/youtube.nix)
+
   ];
 
   homelab = {
--- nixos/sessions/wm/wayland/cage/youtube.nix
+++ nixos/sessions/wm/wayland/cage/youtube.nix
@@ -1,20 +1,68 @@
-{ pkgs, ... }:
 {
-  imports = [ ./. ];
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  ydo = lib.getExe pkgs.ydotool;
+  wob = "${lib.getExe pkgs.wob} -vv";
+  chromium = lib.getExe pkgs.chromium;
+
+  kiosk-launch-script = pkgs.writeShellScriptBin "kiosk-launch" ''
+    export XDG_SESSION_TYPE=wayland
+    export WLR_NO_HARDWARE_CURSORS=1  # Helps if cursor is invisible/glitchy on some Intel iGPUs
+    export YDOTOOL_SOCKET=/run/ydotoold/socket
+
+    # {pkgs.wlr-randr}/bin/wlr-randr --output HDMI-A-1 --mode 1920x1080 --transform 90
+
+    (
+      sleep 2
+      ${ydo} mousemove -a -- 0 0
+    ) &
+
+    # mkfifo /tmp/wobpipe
+    tail -f /tmp/wobpipe | ${wob} &
+
+
+    ${chromium} \
+      --user-data-dir="$XDG_DATA_HOME/chromium/yt-leanback" \
+      --start-fullscreen \
+      --kiosk \
+      --no-first-run \
+      --enable-features=UseOzonePlatform \
+      --ozone-platform=wayland \
+      --disable-restore-session-state \
+      --ignore-gpu-blocklist \
+      --enable-extensions \
+      --user-agent="Mozilla/5.0 (Linux; Android 12) Cobalt/22.2.3-gold (PS4)" \
+      --app="https://youtube.com/tv"
+  '';
+in
+{
+  imports = [ ../.common-wayland.nix ];
+
+  programs.ydotool.enable = true;
+  users.users.max.extraGroups = [ config.programs.ydotool.group ];
+
+  services.input-remapper = {
+    enable = true;
+    enableUdevRules = true;
+  };
+
   services.cage = {
     enable = true;
     user = "max";
-    program = "${pkgs.chromium}/bin/chromium --kiosk --enable-extensions --user-agent='Mozilla/5.0 (PS4; Leanback Shell) Gecko/20100101 Firefox/65.0 LeanbackShell/01.00.01.75 Sony PS4/ (PS4, , no, CH)' https://youtube.com/tv";
-    # program = "${pkgs.chromium}/bin/chromium";
+    program = "${kiosk-launch-script}/bin/kiosk-launch";
+    environment = {
+      WLR_LIBINPUT_NO_DEVICES = "1"; # to also run without any input devices
+    };
     extraArguments = [
       "-s" # Allow VT switching
       "-d" # Don't draw client side decorations, when possible
       # "-m extend" # Extend the display across all connected outputs (default)
       # "-m last" # Use only the last connected output
     ];
-    environment = {
-      WLR_LIBINPUT_NO_DEVICES = "1"; # to also run without any input devices
-    };
   };
-
 }
