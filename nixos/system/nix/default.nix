0daaf6d9aa84f614b01bc8c1c386f5574d4cb27d
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-11 22:29:30 +0100

    feat(nix): add nix config
    disable channels and point nixPatch to flake
    
    add config rev to system builds

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -1,3 +1,4 @@
+{ inputs, lib, ... }:
 {
   nix = {
     settings.experimental-features = [
@@ -5,5 +6,11 @@
       "flakes"
       "pipe-operators"
     ];
+    channel.enable = false;
+    nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
+    # registry.nixpkgs.flake = inputs.nixpkgs;
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
   };
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 }
