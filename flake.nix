8555ce8f03dc7e645d01bf756eb5094ce539f6c5
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-11 22:40:27 +0100

    Revert "feat(flake): remove nixos-hardware"
    This reverts commit 40a575cbcff00341974de57ff2dfdccf8d0dc1ac.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-hardware.url = "nixos-hardware/master";
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
