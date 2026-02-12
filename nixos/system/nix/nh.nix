25b554f292ba7211b0fe804305f23a235cc41b61
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-12 02:08:51 +0100

    feat(nix/nh): enable clean service and enable activation logs for nh

--- nixos/system/nix/nh.nix
+++ nixos/system/nix/nh.nix
@@ -4,9 +4,14 @@
     enable = true;
     flake = flakeRepoRoot;
     clean = {
-      enable = false;
+      enable = true;
       dates = "weekly";
-      extraArgs = "--keep 5 --keep-since 3d";
+      extraArgs = "--keep 5 --keep-since 14d";
     };
+  };
+
+  environment.sessionVariables = {
+    NH_SHOW_ACTIVATION_LOGS = 1;
+    NH_NO_CHECKS = 1;
   };
 }
