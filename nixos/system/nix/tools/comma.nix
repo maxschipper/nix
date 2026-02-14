6e72431bce414c7d77f8d0f1c8fb7bbf2a0cdc7a
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-14 16:40:26 +0100

    feat(comma): use comma and nix-index-database as command-not-found script

--- nixos/system/nix/tools/comma.nix
+++ nixos/system/nix/tools/comma.nix
@@ -1,5 +1,19 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.nix-index-database.nixosModules.nix-index ];
   programs.nix-index-database.comma.enable = true;
+
+  programs.command-not-found.enable = false;
+  environment.systemPackages = [
+    (pkgs.writeTextFile {
+      name = "My-comma-command-not-found-fish";
+      destination = "/share/fish/vendor_functions.d/fish_command_not_found.fish";
+      text = ''
+        function fish_command_not_found
+          echo "Command not found"
+          comma --print-packages $argv
+        end
+      '';
+    })
+  ];
 }
