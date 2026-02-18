ca19577a2e88b423f9ebdce1fd2d651df62f6c64
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-18 13:03:18 +0100

    feat(kanata): update kanata config

--- nixos/services/remap/kanata/default.nix
+++ nixos/services/remap/kanata/default.nix
@@ -1,16 +1,40 @@
+{ pkgs, flakeRepoRoot, ... }:
+let
+  package = pkgs.kanata-with-cmd;
+in
 {
+  environment.systemPackages = [ package ];
   services.kanata = {
-    enable = false;
-    keyboards = {
-      "yoga" = {
-        config = builtins.readFile ./config.kbd;
-        devices = [
-          # "/dev/input/by-id/usb-0000_0000-event-kbd"
-        ];
-        extraArgs = [ ];
-        # extraDefCfg = "danger-enable-cmd yes";
-        port = null;
-      };
+    enable = true;
+    inherit package;
+    keyboards.default = {
+      config = builtins.readFile ./keyboard-config/kanata.kbd;
+      port = null;
+      devices = [
+        "/dev/input/by-path/platform-i8042-serio-0-event-kbd" # yoga keyboard
+      ];
+      extraArgs = [
+        # "--debug"
+        "--cfg"
+        "/etc/kanata/kanata.kbd"
+        "--no-wait"
+      ];
+      extraDefCfg = ''
+        danger-enable-cmd yes
+        log-layer-changes no
+        delegate-to-first-layer no
+        allow-hardware-repeat false
+      '';
     };
+  };
+
+  # bind mount the keymap file to allow live reloading without messing with the security hardening of the systemd unit or with the permissions on my home directory
+  fileSystems."/etc/kanata" = {
+    device = "${flakeRepoRoot}/nixos/services/remap/kanata/keyboard-config";
+    options = [
+      "bind"
+      "ro"
+      "x-systemd.requires-mounts-for=/home"
+    ];
   };
 }
