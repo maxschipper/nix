30ddc7edb2e1944ffe1a244bb622b3ede4b5ca47
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-19 01:17:28 +0100

    feat(nix): use xdg-base-dir and improve registry filtering

--- nixos/system/nix/default.nix
+++ nixos/system/nix/default.nix
@@ -2,16 +2,22 @@
 {
   imports = [ ./caches.nix ];
   nix = {
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-      "pipe-operators"
-    ];
+    settings = {
+      use-xdg-base-directories = true;
+      # warn-dirty = false;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+    };
     channel.enable = false;
     nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
-    # registry.nixpkgs.flake = inputs.nixpkgs;
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
+    registry =
+      inputs
+      |> lib.filterAttrs (name: v: v ? outputs && name != "self")
+      |> lib.mapAttrs (_: flake: { inherit flake; });
   };
   system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
 }
