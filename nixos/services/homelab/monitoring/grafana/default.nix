c8da5d8eaceeecd57fd172ef7c933e1bc87d0913
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-19 02:17:42 +0100

    fix(grafana): add security_key

--- nixos/services/homelab/monitoring/grafana/default.nix
+++ nixos/services/homelab/monitoring/grafana/default.nix
@@ -1,47 +1,55 @@
-{ config, ... }:
+{ config, lib, ... }:
 let
   cfg = config.homelab.services.grafana;
   cfgPrometheus = config.homelab.services.prometheus;
 in
 {
-  services.grafana = {
-    inherit (cfg) enable;
+  config = lib.mkIf {
 
-    dataDir = "/var/lib/grafana";
-    settings = {
-      server = {
-        http_port = cfg.port;
-        http_addr = cfg.ip;
-        root_url = cfg.url;
-        enable_gzip = true;
-      };
-      database = {
-        wal = true;
-        # type = "sqlite3";
-        # path = "/var/lib/grafana/grafana.db";
-      };
-      analytics = {
-        reporting_enabled = false;
-        feedback_links_enabled = false;
-      };
+    sops.secrets."grafana_secret_key" = {
+      owner = "grafana";
     };
-    provision = {
-      enable = true;
-      datasources.settings.datasources = [
-        {
-          name = "Prometheus";
-          type = "prometheus";
-          access = "proxy";
-          editable = true;
-          url = "http://${cfgPrometheus.ip}:${toString cfgPrometheus.port}";
-        }
-        # {
-        #   name = "Loki";
-        #   type = "loki";
-        #   access = "proxy";
-        #   url = "http://127.0.0.1:3100";
-        # }
-      ];
+
+    services.grafana = {
+      inherit (cfg) enable;
+
+      dataDir = "/var/lib/grafana";
+      settings = {
+        security.secret_key = "$__file{${config.sops.secrets."grafana_secret_key".path}}";
+        server = {
+          http_port = cfg.port;
+          http_addr = cfg.ip;
+          root_url = cfg.url;
+          enable_gzip = true;
+        };
+        database = {
+          wal = true;
+          # type = "sqlite3";
+          # path = "/var/lib/grafana/grafana.db";
+        };
+        analytics = {
+          reporting_enabled = false;
+          feedback_links_enabled = false;
+        };
+      };
+      provision = {
+        enable = true;
+        datasources.settings.datasources = [
+          {
+            name = "Prometheus";
+            type = "prometheus";
+            access = "proxy";
+            editable = true;
+            url = "http://${cfgPrometheus.ip}:${toString cfgPrometheus.port}";
+          }
+          # {
+          #   name = "Loki";
+          #   type = "loki";
+          #   access = "proxy";
+          #   url = "http://127.0.0.1:3100";
+          # }
+        ];
+      };
     };
   };
 }
