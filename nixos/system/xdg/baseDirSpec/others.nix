71859a33856e55cfda98c496bc752a16da862cd1
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-19 01:12:29 +0100

    feat(xdg/baseDirSpec): fix env var expansion syntax

--- nixos/system/xdg/baseDirSpec/others.nix
+++ nixos/system/xdg/baseDirSpec/others.nix
@@ -1,16 +1,16 @@
 {
   environment.sessionVariables = {
-    HISTFILE = "$XDG_STATE_HOME/bash/history";
-    XCOMPOSECACHE = "$XDG_CACHE_HOME/X11/xcompose";
-    RUSTUP_HOME = "$XDG_DATA_HOME/rustup";
-    CARGO_HOME = "$XDG_DATA_HOME/cargo";
-    GOPATH = "$XDG_DATA_HOME/go";
-    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java";
-    PARALLEL_HOME = "$XDG_CONFIG_HOME/parallel";
+    HISTFILE = "\${XDG_STATE_HOME}/bash/history";
+    XCOMPOSECACHE = "\${XDG_CACHE_HOME}/X11/xcompose";
+    RUSTUP_HOME = "\${XDG_DATA_HOME}/rustup";
+    CARGO_HOME = "\${XDG_DATA_HOME}/cargo";
+    GOPATH = "\${XDG_DATA_HOME}/go";
+    _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=\${XDG_CONFIG_HOME}/java";
+    PARALLEL_HOME = "\${XDG_CONFIG_HOME}/parallel";
     PYTHON_HISTORY = "";
-    NPM_CONFIG_INIT_MODULE = "$XDG_CONFIG_HOME/npm/config/npm-init.js";
-    NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
-    NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
-    DOTNET_CLI_HOME = "$XDG_DATA_HOME/dotnet";
+    NPM_CONFIG_INIT_MODULE = "\${XDG_CONFIG_HOME}/npm/config/npm-init.js";
+    NPM_CONFIG_CACHE = "\${XDG_CACHE_HOME}/npm";
+    NPM_CONFIG_TMP = "\${XDG_RUNTIME_DIR}/npm";
+    DOTNET_CLI_HOME = "\${XDG_DATA_HOME}/dotnet";
   };
 }
